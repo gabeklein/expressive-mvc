1042f9c994776b2613168d5ee3c7d432b20e8a49
Gabe <gabe@termtm.com>
2019-08-10 01:52:28 -0400

    reflect changes to runtime in type definition

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,7 @@ interface BunchOf<T> {
  * 
  */
 interface LiveState {
+
     /**
      * Trigger update of consumer component.
      * 
@@ -55,6 +56,19 @@ type InitStateOnMount<I extends BunchOf<any>, S = LiveState & I> =
     (this: S, onUnmount: (cb: VoidFunction) => void, state: S) => I
 
 /**
+ * LiveState Controller Hook.
+ * 
+ * @param init Controller Class
+ * Class properties determine what values are live. 
+ * 
+ * Note: Properties starting with _ will not trigger any re-renders.
+ * 
+ * @returns {LiveState} Live state: interactive state of component.
+ * 
+ */
+declare function use<I, A extends any[]>(init: { new (...args: A): I; }, ...args: A): LiveState & I;
+
+/**
  * LiveState React Hook 
  * Returned object sets initial state, and determines what values are live.
  * 
@@ -63,37 +77,16 @@ type InitStateOnMount<I extends BunchOf<any>, S = LiveState & I> =
  * 
  * Initializer function which returns state at mount. Runs only once.
  * 
- * @param {InitStateOnMount} init Initital State or initializer. (Functionally `ComponentDidMount`) 
+ * @param init Initital values or initializer (returning those values) of resulting state.
  * 
  * @returns {LiveState} Live state: current state of component.
  * 
  */
-declare function use
-    <I, S = LiveState & I> (
-    init: (this: S, onUnmount: (cb: VoidFunction) => void, state: S) => I
-): I;
-
-/**
- * LiveState React Hook.
- * 
- * Any value initialized will be included as getter-setter pair on returned object.
- * Updates to values on state object will trigger a re-render.
- * 
- * Accepts object (initializer) or function which returns initializer.
- * Init function is called only on mount, so may be used as such.
- * 
- * @param init State initializer.
- * Sets initial, and determines what values are live.
- * 
- * @returns {LiveState} Live state: current state of component.
- * 
- */
-declare function use
-    <I extends BunchOf<any>>
-    (init: I): LiveState & I;
+declare function use<I, A extends any[]>(init: (...args: A) => I, ...args: A): LiveState & I;
+declare function use<I>(init: I): LiveState & I;
 
 export { 
     use, 
-    use as useStates, 
+    use as useStates,
     use as useController
 }
