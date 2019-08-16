a694c1f7fc7fb08b418f6fe3043f313c327b1cdb
Gabe <gabe@termtm.com>
2019-08-16 02:44:36 -0400

    style fixes

--- examples/src/common.jsx
+++ examples/src/common.jsx
@@ -15,21 +15,23 @@ const Style = () => (
       margin: 0;
       padding: 0;
       color: #567ecc;
-      font-size: 60px;
+      font-size: 40px;
     }
     #container {
       height: 100vh;
       display: flex;
+      flex-direction: column;
       align-items: center;
       justify-content: center;
     }
     .clicky {
       padding: .2em .4em;
+      margin: .5em;
       border-radius: .2em;
       background: #eee;
       cursor: pointer;
       user-select: none;
-      text-align: middle;
+      vertical-align: middle;
     }
   `}</style>
 )
