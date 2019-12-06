9e5f21f5938f38817ed6c58d0b9dacb99880049c
Gabe <gabe@termtm.com>
2019-12-06 05:43:06 -0500

    tidy up Controller's factory code

--- src/controller.ts
+++ src/controller.ts
@@ -1,23 +1,14 @@
-import {
-  getControlProvider,
-  getFromController,
-  ownContext,
-  subToController,
-  tapFromController,
-} from './context';
+import { getControlProvider, getFromController, ownContext, subToController, tapFromController } from './context';
 import { applyExternal, ensureDispatch, NEW_SUB } from './dispatch';
 import { createWrappedComponent } from './provider';
 import { ModelController } from './types';
 import { useOwnController } from './use_hook';
 
 const { 
-  defineProperty: define 
+  defineProperty: define,
+  defineProperties: defineAll
 } = Object;
 
-function returnThis(this: any){ return this }
-function getContext(this: typeof ModelController){ return ownContext(this) }
-
-/** Just the host function, nothing initialized here */
 export function Controller(this: ModelController){
   if(this.didInit)
     setImmediate(() => this.didInit!())
@@ -29,7 +20,7 @@ for(const f of ["on", "not", "only", "once"])
   prototype[f] = returnThis;
 
 prototype.watch = applyExternal;
-prototype.willDestroy = function(cb?: () => void){ if(cb) cb() };
+prototype.willDestroy = runCallback;
 
 define(prototype, NEW_SUB, {
   get: ensureDispatch,
@@ -40,17 +31,32 @@ define(prototype, "Provider", {
   get: getControlProvider
 })
 
-Controller.sub = subToController;
-Controller.get = getFromController;
-Controller.tap = tapFromController;
+defineAll(Controller, {
+  use: { value: useController },
+  sub: { value: subToController },
+  get: { value: getFromController },
+  tap: { value: tapFromController },
+  hoc: { value: createWrappedComponent },
+  context: { value: getContext },
+  Provider: { get: getProvider }
+})
 
-Controller.context = getContext;
-Controller.hoc = createWrappedComponent;
-
-Controller.use = function use(...args: any[]){
-  return useOwnController(this, args);
+function returnThis(this: any){ 
+  return this
 }
 
-define(Controller, "Provider", {
-  get(){ return useOwnController(this).Provider }
-})
+function runCallback(cb?: () => void){
+  if(cb) cb()
+}
+
+function getContext(this: typeof ModelController){ 
+  return ownContext(this)
+}
+
+function getProvider(this: typeof ModelController){
+  return useOwnController(this).Provider
+}
+
+function useController(this: typeof ModelController, ...args: any[]){
+  return useOwnController(this, args) 
+}
