6f3855bd3e65f0a0d6a12a64174a2148d28e232c
Gabe Klein <gabe@termtm.com>
2020-08-22 15:59:28 -0600

    add jsdocs to ambient types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,15 @@
+/**
+ * Any object containing arbirary properties of type {T}.
+ */
 type BunchOf<T> = { [key: string]: T };
+
+/**
+ * Any function to fire off some expected action.
+ */
 type Callback = () => void;
+
+/**
+ * Workaround to retreive `typeof this`
+ * in static methods using generics.
+ */
 type Class = new(...args: any[]) => any;
