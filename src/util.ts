a78da7466c60984af4005fe52b8e5999a7bf9113
Gabe Klein <gabe@termtm.com>
2020-08-22 02:45:51 -0600

    remove unused util functions

--- src/util.ts
+++ src/util.ts
@@ -1,24 +1,5 @@
 import { Controller } from './controller';
 
-export class Map<K, V> extends Array<[K, V]> {
-  get = (key: K) => {
-    for(const [ _key, value ] of this)
-      if(key === _key)
-        return value as V;
-  }
-
-  set = (key: K, value: V) => {
-    for(let i=0, l=this.length; i < l; i++)
-      if(this[i][0] === key){
-        this[i][1] = value;
-        return;
-      }
-    
-    this.push([key, value]);
-    return;
-  }
-}
-
 export function define(target: {}, values: {}): void;
 export function define(target: {}, key: string | symbol, value: any): void;
 export function define(target: {}, kv: {} | string | symbol, v?: {}){
@@ -27,23 +8,6 @@ export function define(target: {}, kv: {} | string | symbol, v?: {}){
   else
     for(const [key, value] of Object.entries(kv))
       Object.defineProperty(target, key, { value });
-}
-
-export function callIfExists<T, A extends any[]>(
-  fn?: (...args: A) => T, on?: any, args?: A){
-    
-  return fn && fn.apply(on, args as any)
-}
-
-export function dedent(t: TemplateStringsArray, ...v: any[]): string {
-  const text = v.reduce((a, v, i) => a + v + t[i + 1], t[0]);
-  const starting = /^\n( *)/.exec(text);
-  
-  if(starting){
-    const indent = new RegExp("\n" + starting[1], "g");
-    return text.replace(starting[0], "").replace(indent, "\n").replace(/\s*\n*$/, "")
-  } 
-  else return text;
 }
 
 export function defineOnAccess<T>(
