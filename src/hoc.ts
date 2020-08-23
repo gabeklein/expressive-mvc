9b2dd6e67f6f8064e442dd64cd3fe327e45764f2
Gabe Klein <gabe@termtm.com>
2020-08-23 13:12:36 -0600

    optimize controlled value component

--- src/hoc.ts
+++ src/hoc.ts
@@ -1,7 +1,6 @@
 import { createElement, FC, PropsWithChildren, useEffect, useState } from 'react';
 
 import { Controller, within } from './controller';
-import { useManualRefresh } from './hook';
 import { getObserver } from './observer';
 import { useModelController } from './subscriber';
 
@@ -23,13 +22,13 @@ export function createWrappedComponent(
 }
 
 export function ControlledValue(this: Controller): FC<{ of: string }> {
-  return ({ of: target, ...props }) => {
-    const onDidUpdate = useManualRefresh()[1];
+  return ({ of: key, ...props }) => {
+    const [ value, onUpdate ] = useState(() => within(this, key));
     
     useEffect(() => {
-      return getObserver(this).addListener(target, onDidUpdate);
+      return getObserver(this).watch(key, onUpdate);
     }, [])
 
-    return createElement("span", props, within(this, target))
+    return createElement("span", props, value);
   }
 }
