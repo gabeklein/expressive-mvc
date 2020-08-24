933e09b544b9d0ce4f3f26f742b311e2ae3ef323
Gabe Klein <gabe@termtm.com>
2020-08-24 02:48:12 -0600

    reorder subscriber functions

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -16,6 +16,45 @@ function useManualRefresh(){
   return state[0];
 }
 
+export function useSimpleSubscriber<T extends Observable>
+  (control: T){
+
+  const onShouldUpdate = useManualRefresh();
+
+  const subscription = useMemo(() => {
+    return new Subscription(control, onShouldUpdate);
+  }, []);
+
+  useEffect(() => {
+    subscription.start();
+    return () => subscription.stop();
+  }, []);
+
+  return subscription.proxy;
+}
+
+export function useActiveSubscriber<T extends Controller>
+  (target: T, args: any[], main: boolean){
+
+  let initial = false;
+
+  const onShouldUpdate = useManualRefresh();
+
+  const subscription = useMemo(() => {
+    initial = true;
+    target.ensureDispatch();
+
+    return new Subscription(target, onShouldUpdate);
+  }, []);
+
+  useLifecycleEffect((name) => {
+    subscription.handleEvent(name);
+    hitLifecycle(target, name, args, main);
+  }, initial);
+  
+  return subscription.proxy;
+}
+
 export function useModelController<T extends typeof Controller>(
   model: T,
   args?: any[], 
@@ -54,45 +93,6 @@ export function useModelController<T extends typeof Controller>(
       instance.destroy();
     }
   }, initial);
-
-  return subscription.proxy;
-}
-
-export function useActiveSubscriber<T extends Controller>
-  (target: T, args: any[], main: boolean){
-
-  let initial = false;
-
-  const onShouldUpdate = useManualRefresh();
-
-  const subscription = useMemo(() => {
-    initial = true;
-    target.ensureDispatch();
-
-    return new Subscription(target, onShouldUpdate);
-  }, []);
-
-  useLifecycleEffect((name) => {
-    subscription.handleEvent(name);
-    hitLifecycle(target, name, args, main);
-  }, initial);
-  
-  return subscription.proxy;
-}
-
-export function useSimpleSubscriber<T extends Observable>
-  (control: T){
-
-  const onShouldUpdate = useManualRefresh();
-
-  const subscription = useMemo(() => {
-    return new Subscription(control, onShouldUpdate);
-  }, []);
-
-  useEffect(() => {
-    subscription.start();
-    return () => subscription.stop();
-  }, []);
 
   return subscription.proxy;
 }
