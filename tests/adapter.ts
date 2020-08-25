cdb8f78d6463258d03109f872d71654692ab6765
Gabe Klein <gabe@termtm.com>
2020-08-25 01:19:09 -0600

    remove support for abstracted tests

--- tests/adapter.ts
+++ tests/adapter.ts
@@ -1,13 +1,7 @@
 import { renderHook, RenderHookResult } from '@testing-library/react-hooks';
 
+//TODO: removable?
 import { SUBSCRIPTION } from '../src/subscription';
-
-type Class = new (...args: any[]) => any;
-type Initializer = () => any;
-
-function ensureArray<T>(x: T | T[]){
-  return ([] as T[]).concat(x);
-}
 
 const frame = / *at ([^\/].+?)?(?: \()?(\/[\/a-zA-Z-_.]+):(\d+):(\d+)/;
 
@@ -47,114 +41,14 @@ class TraceableError extends Error {
 }
 
 /**
- * Test-definition, activate Controller hook on a given class.
- */
-interface TestSuite<T extends Class> {
-  /** Activate controller. Passes `args` to constructor */
-  use?: T;
-  /** forward these arguments to the activated method */
-  args?: any[];
-
-  /** Subscribe to this, plus whatever automatic inference finds */
-  on?: string | string[];
-  /** Don't subscribe to this, despite automatic inference */
-  not?: string | string[];
-  /** Only subscribe to this, override automatic inference */
-  only?: string | string[];
-  /** Disable automatic inference */
-  once?: true;
-
-  /** 
-   * Access these properties on first render; 
-   * this simulates a destructure so automatic 
-   * subscription can infer what to watch. 
-   */
-  peek?: string | string[];
-}
-
-/**
  * Test a ModelController with this. Equivalent to `renderHook`, 
  * however for controller hooks.
- * 
- * Pass in object with desired hook, and an applicable class as it's value. 
- * 
- * Except for `use`, given class must extend `Controller`
- * 
- * e.g. `{ use: TestController }`
- * 
- * `peek` simulates access on first render.
- * 
- * `args` passes arguments to `Controller.use()`
- * 
- * Available Hooks:
- * - use
- * 
- * Refresh Directives:
- * - on
- * - only
- * - not
- * - once
  */
-export function trySubscribe<T extends Class>(config: TestSuite<T>): RenderControllerResult<InstanceType<T>>
-export function trySubscribe<T>(init: () => T): RenderControllerResult<T>
-export function trySubscribe(config: TestSuite<any> | Initializer){
-  let init: Initializer;
+export function trySubscribe<T>(
+  init: () => T): RenderControllerResult<T>{
 
-  if(typeof config === "function")
-    init = config;
-
-  else {
-    const Model = config.use;
-
-    if(!Model)
-      throw new Error(`\`use: Controller\` not specified in test suite!`);
-
-    init = () => Model.use();
-    init = adjustSubscription(init, config);
-
-    if(config.peek)
-      init = mockPropertyAccess(init, config.peek);
-  }
-
-  let api = renderHook(init);
-  
+  const api = renderHook(init);
   return plusUpdateAssertions(api);
-}
-
-function mockPropertyAccess(
-  chain: Initializer, 
-  vars: string | string[] ){
-
-  return () => {
-    const ref = chain();
-
-    for(const key of ensureArray(vars))
-      void ref[key]; 
-    
-    ref[SUBSCRIPTION].start();
-
-    return Object.getPrototypeOf(ref);
-  }
-}
-
-function adjustSubscription(
-  chain: Initializer, 
-  { once, only, on, not }: TestSuite<any>){
-
-  if(once)
-    return () => chain().once();
-  else if(only)
-    return () => chain().only(...ensureArray(only))
-  else {
-    if(on){
-      const link = chain;
-      chain = () => link().on(...ensureArray(on))
-    }
-    if(not)
-      return () => chain().not(...ensureArray(not))
-    else
-      return chain;
-  }
 }
 
 interface RenderControllerResult<T> 
