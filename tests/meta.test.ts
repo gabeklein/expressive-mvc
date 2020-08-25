67274c27d647c41dbb06948d77fd25636acd4c36
Gabe Klein <gabe@termtm.com>
2020-08-25 00:54:44 -0600

    seperate meta-related tests

---
+++ tests/meta.test.ts
@@ -0,0 +1,65 @@
+import Controller from './lib';
+import { trySubscribe } from './adapter';
+
+class Child extends Controller {
+  value: string;
+
+  constructor(value: string){
+    super();
+    this.value = value;
+  }
+}
+
+class Parent extends Controller {
+  static foo = "bar";
+  static bar = new Child("meta");
+}
+
+test('tracks static values on meta', async () => {
+  const { state, assertDidUpdate } =
+    trySubscribe(() => {
+      const instance = Parent.meta();
+
+      //simulate destructure access
+      void instance.foo;
+
+      return instance;
+    });
+
+  expect(state.foo).toBe("bar");
+
+  state.foo = "foo";
+
+  await assertDidUpdate();
+  expect(state.foo).toBe("foo");
+})
+
+test('tracks nested values on meta', async () => {
+  const { state, assertDidUpdate } =
+    trySubscribe(() => {
+      const instance = Parent.meta();
+
+      //simulate destructure access
+      void instance.bar;
+      void instance.bar.value;
+
+      return instance;
+    });
+
+  expect(state.bar.value).toBe("meta");
+
+  // Will refresh on sub-value change.
+  state.bar.value = "bar";
+  await assertDidUpdate();
+  expect(state.bar.value).toBe("bar");
+
+  // Will refresh on repalcement.
+  state.bar = new Child("foo");
+  await assertDidUpdate();
+  expect(state.bar.value).toBe("foo");
+
+  // Fresh subscription does still work.
+  state.bar.value = "bar";
+  await assertDidUpdate();
+  expect(state.bar.value).toBe("bar");
+})
--- tests/nesting.test.ts
+++ tests/nesting.test.ts
@@ -2,73 +2,32 @@ import Controller from './lib';
 import { trySubscribe } from './adapter';
 
 class Child extends Controller {
-  bar = "foo";
+  value = "foo"
 }
 
 class Parent extends Controller {
-  static hello = "world";
-  static foo = new Child();
-  
   foo = "bar";
-  child = new Child();
+  bar = new Child();
 }
 
-test('tracks nested controllers', async () => {
-  const { state, assertDidUpdate } = trySubscribe(() => {
-    const instance = Parent.use();
-    void instance.foo;
-    void instance.child.bar;
-    return instance;
-  });
+test('tracks values of nested controllers', async () => {
+  const { state, assertDidUpdate } = 
+    trySubscribe(() => {
+      const instance = Parent.use();
+
+      //simulate destructure access
+      void instance.foo;
+      void instance.bar.value;
+
+      return instance;
+    });
 
   expect(state.foo).toBe("bar");
-  expect(state.child.bar).toBe("foo");
+  expect(state.bar.value).toBe("foo");
 
   state.foo = "foo";
   await assertDidUpdate();
 
-  state.child.bar = "bar";
+  state.bar.value = "bar";
   await assertDidUpdate();
-})
-
-test('tracks static values on meta', async () => {
-  const { state, assertDidUpdate } = trySubscribe(() => {
-    const instance = Parent.meta();
-    void instance.hello;
-    return instance;
-  });
-
-  expect(state.hello).toBe("world");
-
-  state.hello = "World!";
-
-  await assertDidUpdate();
-
-  expect(state.hello).toBe("World!");
-})
-
-test('tracks controller values on meta', async () => {
-  const { state, assertDidUpdate } = trySubscribe(() => {
-    const instance = Parent.meta();
-    void instance.foo;
-    void instance.foo.bar;
-    return instance;
-  });
-
-  expect(state.foo.bar).toBe("foo");
-
-  // Will refresh on sub-value change.
-  state.foo.bar = "bar";
-  await assertDidUpdate();
-  expect(state.foo.bar).toBe("bar");
-
-  // Will refresh on repalcement.
-  state.foo = new Child();
-  await assertDidUpdate();
-  expect(state.foo.bar).toBe("foo");
-
-  // Fresh subscription does work.
-  state.foo.bar = "bar";
-  await assertDidUpdate();
-  expect(state.foo.bar).toBe("bar");
 })
