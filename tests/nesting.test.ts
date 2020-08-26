42b7d59565f54f540d5d37fd39cf5d4d46d8bbfc
Gabe Klein <gabe@termtm.com>
2020-08-25 22:24:05 -0600

    add test to nesting

--- tests/nesting.test.ts
+++ tests/nesting.test.ts
@@ -22,3 +22,25 @@ it('tracks values of nested controllers', async () => {
   state.child.value = "bar";
   await assertDidUpdate();
 })
+
+it('tracks a new child as update to nested value', async () => {
+  const { state, assertDidUpdate } = 
+    test(Parent, ["child", "child.value"]);
+
+  expect(state.child.value).toBe("foo");
+
+  // Will refresh on sub-value change.
+  state.child.value = "bar";
+  await assertDidUpdate();
+  expect(state.child.value).toBe("bar");
+
+  // Will refresh on repalcement.
+  state.child = new Child();
+  await assertDidUpdate();
+  expect(state.child.value).toBe("foo");
+
+  // Fresh subscription does still work.
+  state.child.value = "bar";
+  await assertDidUpdate();
+  expect(state.child.value).toBe("bar");
+})
