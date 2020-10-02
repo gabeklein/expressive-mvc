79e45c7e64efc4b21a0f9bb5d43c3370900bf0f0
Gabe Klein <gabe@termtm.com>
2020-10-02 05:11:55 -0600

    reorganize Controller class

--- src/controller.ts
+++ src/controller.ts
@@ -28,77 +28,7 @@ export interface Controller
 }
 
 export class Controller {
-  tap(...path: maybeStrings){
-    return usePassiveSubscriber(this, ...path);
-  }
-
-  sub(...args: any[]){
-    return useActiveSubscriber(this, args);
-  }
-
-  destroy(){
-    const dispatch = observe(this);
-
-    if(dispatch)
-      dispatch.emit("willDestroy");
-    
-    if(this.willDestroy)
-      this.willDestroy();
-  }
-
-  applyDispatch(observer: Observer){
-    observer.monitorValues();
-    observer.monitorComputed(Controller);
-    observer.mixin();
   
-    if(this.didCreate)
-      this.didCreate();
-  }
-
-  static hoc = createWrappedComponent;
-
-  static get Provider(){
-    return useOwnController(this).Provider;
-  }
-
-  static isTypeof<T extends Class>(this: T, maybe: any): maybe is T {
-    return (
-      !!maybe && 
-      typeof maybe == "object" && 
-      maybe.prototype instanceof this
-    )
-  }
-
-  static find(){
-    return getFromContext(this);
-  }
-
-  static meta(...path: maybeStrings): any {
-    return usePassiveSubscriber(this, ...path);
-  }
-
-  static create<T extends Class>(
-    this: T,
-    args?: any[],
-    prepare?: (self: InstanceType<T>) => void){
-
-    const instance: InstanceType<T> = 
-      new (this as any)(...args || []);
-
-    if(prepare)
-      prepare(instance);
-
-    observe(instance);
-    
-    return instance;
-  }
-
-  static applyDispatch(observer: Observer){
-    observer.monitorValues(Function);
-    observer.monitorComputed(Controller);
-    observer.mixin();
-  }
-
   static use(...args: any[]){
     return useOwnController(this, args);
   }
@@ -127,8 +57,16 @@ export class Controller {
     return subscriber;
   }
 
+  static find(){
+    return getFromContext(this);
+  }
+
   static get(key?: string){
     return usePassiveGetter(this.find(), key);
+  }
+
+  public tap(...path: maybeStrings){
+    return usePassiveSubscriber(this, ...path);
   }
 
   static tap(...keys: maybeStrings){
@@ -136,7 +74,7 @@ export class Controller {
   }
 
   static has(key: string){
-    const value = this.tap(key);
+    const value = this.find().tap(key);
 
     if(value === undefined)
       throw Oops.HasPropertyUndefined(this.name, key);
@@ -144,9 +82,71 @@ export class Controller {
     return value;
   }
 
+  public sub(...args: any[]){
+    return useActiveSubscriber(this, args);
+  }
+
   static sub(...args: any[]){
-    const instance = this.find();
-    return useActiveSubscriber(instance, args);
+    return this.find().sub(...args);
+  }
+
+  static meta(...path: maybeStrings): any {
+    return usePassiveSubscriber(this, ...path);
+  }
+
+  public applyDispatch(observer: Observer){
+    observer.monitorValues();
+    observer.monitorComputed(Controller);
+    observer.mixin();
+  
+    if(this.didCreate)
+      this.didCreate();
+  }
+
+  static applyDispatch(observer: Observer){
+    observer.monitorValues(Function);
+    observer.monitorComputed(Controller);
+    observer.mixin();
+  }
+
+  static create<T extends Class>(
+    this: T,
+    args?: any[],
+    prepare?: (self: InstanceType<T>) => void){
+
+    const instance: InstanceType<T> = 
+      new (this as any)(...args || []);
+
+    if(prepare)
+      prepare(instance);
+
+    observe(instance);
+    
+    return instance;
+  }
+
+  public destroy(){
+    const dispatch = observe(this);
+
+    if(dispatch)
+      dispatch.emit("willDestroy");
+    
+    if(this.willDestroy)
+      this.willDestroy();
+  }
+
+  static hoc = createWrappedComponent;
+
+  static get Provider(){
+    return this.use().Provider;
+  }
+
+  static isTypeof<T extends Class>(this: T, maybe: any): maybe is T {
+    return (
+      !!maybe && 
+      typeof maybe == "object" && 
+      maybe.prototype instanceof this
+    )
   }
 }
 
