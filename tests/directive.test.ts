196568a47b4601292d8b657b177c31dfe61f69dc
Gabe Klein <gabe@termtm.com>
2020-10-10 16:02:17 -0600

    combine ref tests with directives

--- tests/directive.test.ts
+++ tests/directive.test.ts
@@ -1,4 +1,4 @@
-import Controller, { test, set, use } from "./adapter";
+import Controller, { ref, set, test, use } from './adapter';
 
 describe("set Directive", () => {
   class Subject extends Controller {
@@ -70,5 +70,57 @@ describe("use Directive", () => {
 
   it('ran child callback on create', () => {
     expect(parent.hello).toBe(WORLD);
+  })
+})
+
+describe("ref Directive", () => {
+  class Subject extends Controller {
+    checkValue?: any = undefined;
+  
+    ref1 = ref<string>();
+  
+    ref2 = ref<symbol>(value => {
+      this.checkValue = value;
+    })
+  
+    ref3 = ref<number>(() => {
+      return () => {
+        this.checkValue = true;
+      }
+    })
+  }
+  
+  it('watches "current" of ref property', async () => {
+    const { state, assertDidUpdate } = test(Subject, ["ref1"]);
+    const callback = jest.fn()
+  
+    state.once("ref1", callback);
+    state.ref1.current = "value1";
+    await assertDidUpdate();
+    expect(callback).toBeCalledWith("value1", "ref1");
+  })
+  
+  it('invokes callback of ref property', async () => {
+    const { state, assertDidUpdate } = test(Subject, ["ref2"]);
+    const targetValue = Symbol("inserted object");
+    const callback = jest.fn();
+  
+    expect(state.checkValue).toBe(undefined);
+    state.once("ref2", callback);
+    state.ref2.current = targetValue;
+    expect(state.checkValue).toBe(targetValue);
+    await assertDidUpdate();
+    expect(callback).toBeCalledWith(targetValue, "ref2");
+  })
+  
+  it('invokes callback on ref overwrite', async () => {
+    const { state, assertDidUpdate } = test(Subject, ["ref3"]);
+  
+    state.ref3.current = 1;
+    await assertDidUpdate();
+    expect(state.checkValue).toBe(undefined);
+    state.ref3.current = 2;
+    await assertDidUpdate();
+    expect(state.checkValue).toBe(true);
   })
 })
