dc71a371b56d215a1cd1f929ce222b382c13929b
Gabe Klein <gabe@termtm.com>
2020-11-14 20:06:05 -0700

    clean up directives

--- src/directives.ts
+++ src/directives.ts
@@ -8,72 +8,78 @@ import Oops from './issues';
 
 export class Placeholder {
   constructor(
-    public applyTo: (recipient: Observer, as: string) => void
+    public applyTo: (recipient: Observer, key: string) => void
   ){}
-
-  static is(fn: (on: Observer, key: string) => void){
-    return new this(fn) as any;
-  }
 }
 
 export function childProperty<T extends Model>
   (Peer: T, callback?: (i: State<T>) => void): State<T> {
 
-  return Placeholder.is((on: Observer, as: string) => {
+  function bindChild(on: Observer, key: string){
     const parent = on.subject;
     const instance = Peer.create();
     define(instance, { parent });
-    define(parent, as, instance);
+    define(parent, key, instance);
     if(callback)
       callback(instance);
-  })
+  }
+
+  return new Placeholder(bindChild) as any;
 }
 
 export function peerProperty<T extends Model>
   (Peer: T): State<T> {
 
-  return Placeholder.is((on: Observer, as: string) => {
+  function bindSibling(on: Observer, key: string){
     const subject = on.subject as Controller;
 
     if(Singleton.isTypeof(Peer))
-      defineLazy(subject, as, () => Peer.find());
+      defineLazy(subject, key, () => Peer.find());
     else if(subject instanceof Singleton)
       throw Oops.CantAttachGlobal(subject.constructor.name, Peer.name);
     else
-      define(subject, as, Peer);
-  })
+      define(subject, key, Peer);
+  }
+
+  return new Placeholder(bindSibling) as any;
 }
 
 export function refProperty<T = any>
   (effect?: EffectCallback<Controller, any>): RefObject<T> {
 
-  return Placeholder.is((on: Observer, as: string) => {
-    const descriptor = on.access(as, effect);
+  function createReference(on: Observer, key: string){
+    const descriptor = on.access(key, effect);
 
-    defineProperty(on.subject, as, {
+    defineProperty(on.subject, key, {
       enumerable: true,
       value: defineProperty({}, "current", descriptor)
     });
-  })
+  }
+
+  return new Placeholder(createReference) as any;
 }
 
 export function effectProperty<T = any>
   (effect: EffectCallback<Controller, any>): T {
 
-  return Placeholder.is((on: Observer, as: string) => {
-    const descriptor = on.access(as, effect);
+  function registerEffect(on: Observer, key: string){
+    const descriptor = on.access(key, effect);
 
-    defineProperty(on.subject, as, {
+    defineProperty(on.subject, key, {
       enumerable: true,
       ...descriptor
     });
-  })
+  }
+
+  return new Placeholder(registerEffect) as any;
 }
 
 export function eventProperty
   (callback?: EffectCallback<Controller>){
 
-  return Placeholder.is((on: Observer, as: string) => {
-    on.monitorEvent(as, callback);
-  })
+  function registerEvent(on: Observer, key: string){
+    on.monitorEvent(key, callback);
+  }
+
+  return new Placeholder(registerEvent) as any;
 }
