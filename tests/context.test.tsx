31eb20c5ad62956835c88b958f5638fb0ed93354
Gabe Klein <gabe@termtm.com>
2020-11-27 02:12:40 -0500

    simplify context tests

--- tests/context.test.tsx
+++ tests/context.test.tsx
@@ -3,113 +3,92 @@ import { create } from "react-test-renderer";
 
 import { Consumer, Controller, Provider } from "./adapter";
 
-class Foo extends Controller {
-  foo = "foo";
-}
-
-class Bar extends Controller {
-  bar = "bar"
-}
-
-class Baz extends Bar {
-  bar = "foobar";
-  baz = "baz";
-}
-
-function FooProvider({ children }: any){
-  const { Provider } = Foo.use();
-  return (
-    <Provider>{children}</Provider>
-  );
-}
+class Foo extends Controller {}
+class Bar extends Controller {}
+class Baz extends Bar {}
 
 describe("Provider", () => {
   it("provides an existing instance of controller", () => {
-    const log = jest.fn();
+    const instance = Foo.create();
 
     create(
-      <FooProvider>
-        <Consumer of={Foo} got={i => log(i.foo)}/>
-      </FooProvider>
+      <instance.Provider>
+        <Consumer of={Foo} got={i => {
+          expect(i).toStrictEqual(instance);
+        }}/>
+      </instance.Provider>
     );
-
-    expect(log).toBeCalledWith("foo");
   })
 
   it("creates an instance of parent controller", () => {
-    const log = jest.fn();
-
     create(
       <Foo.Provider>
-        <Consumer of={Foo} got={i => log(i.foo)}/>
+        <Consumer of={Foo} got={i => {
+          expect(i).toBeInstanceOf(Foo);
+        }}/>
       </Foo.Provider>
     );
-
-    expect(log).toBeCalledWith("foo");
   })
 
   it("creates multiple instances via MultiProvider", () => {
-    const log = jest.fn();
-    
     create(
       <Provider of={{ Foo, Bar }}>
-        <Consumer of={Foo} got={i => log(i.foo)}/>
-        <Consumer of={Bar} got={i => log(i.bar)}/>
+        <Consumer of={Foo} got={i => {
+          expect(i).toBeInstanceOf(Foo);
+        }}/>
+        <Consumer of={Bar} got={i => {
+          expect(i).toBeInstanceOf(Bar);
+        }}/>
       </Provider>
     )
-
-    expect(log).toBeCalledWith("foo");
-    expect(log).toBeCalledWith("bar");
   })
 })
 
 describe("Consumer", () => {
   it("can handle complex arrangement", () => {
-    const log = jest.fn();
-    
+    const instance = Foo.create();
+
     create(
-      <FooProvider>
+      <instance.Provider>
         <Baz.Provider>
           <Provider of={{ Bar }}>
-            <Consumer of={Foo} got={i => log(i.foo)}/>
-            <Consumer of={Bar} got={i => log(i.bar)}/>
-            <Consumer of={Baz} got={i => log(i.baz)}/>
+            <Consumer of={Foo} got={i => {
+              expect(i).toStrictEqual(instance);
+            }}/>
+            <Consumer of={Bar} got={i => {
+              expect(i).toBeInstanceOf(Bar);
+            }}/>
+            <Consumer of={Baz} got={i => {
+              expect(i).toBeInstanceOf(Baz);
+            }}/>
           </Provider>
         </Baz.Provider>
-      </FooProvider>
+      </instance.Provider>
     )
-
-    expect(log).toBeCalledWith("foo");
-    expect(log).toBeCalledWith("bar");
-    expect(log).toBeCalledWith("baz");
   })
 
   it("may select a super-instance instead", () => {
-    const log = jest.fn();
-    
     create(
       <Baz.Provider>
-        <Consumer of={Bar} got={i => log(i.bar)}/>
+        <Consumer of={Bar} got={i => {
+          expect(i).toBeInstanceOf(Baz);
+        }}/>
       </Baz.Provider>
     )
-
-    expect(log).toBeCalledWith("foobar");
   })
 
   it("prefers closest match over best match", () => {
-    const log = jest.fn();
-    
     create(
       <Bar.Provider>
         <Baz.Provider>
-          <Consumer of={Baz} got={i => log(i.bar)}/>
-          <Consumer of={Bar} got={i => log(i.bar)}/>
+          <Consumer of={Baz} got={i => {
+            expect(i).toBeInstanceOf(Baz);
+          }}/>
+          <Consumer of={Bar} got={i => {
+            expect(i).toBeInstanceOf(Baz);
+          }}/>
         </Baz.Provider>
       </Bar.Provider>
     )
-
-    expect(log).toBeCalledTimes(2);
-    expect(log).toBeCalledWith("foobar");
-    expect(log).not.toBeCalledWith("bar");
   })
 })
