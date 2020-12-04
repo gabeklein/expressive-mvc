4a1a5a9494f5fc2d9da1d9151df87e93cde987df
Gabe Klein <gabe@termtm.com>
2020-12-03 23:37:34 -0700

    add tests for override computed

--- tests/extends.test.ts
+++ tests/extends.test.ts
@@ -1,38 +1,73 @@
 import Controller, { test } from "./adapter";
 
-class Subject extends Controller {
-  value = 1;
-  value2 = 2;
+describe("computed", () => {
+  class Parent extends Controller {
+    value = 1;
+    value2 = 2;
 
-  constructor(
-    public init?: string){
-    super();
+    get value3(){
+      return this.value + this.value2;
+    }
+
+    get value4(){
+      return "foo";
+    }
   }
 
-  setValueToThree = () => {
-    this.value = 3;
+  class Child extends Parent {
+    value3 = 4;
+
+    get value4(){
+      return "bar";
+    }
   }
-}
 
-it('initializes from extended Controller', () => {
-  const { state } = test(Subject);
-
-  expect(state.value).toBe(1);
-  expect(state.value2).toBe(2);
-})
-
-it('passes arguments to constructor', () => {
-  const { state } = test(() => {
-    return Subject.use("Hello World!")
+  it("allowes inheritors to override", () => {
+    const instance = Child.create();
+    expect(instance.value3).toBe(4);
   })
 
-  expect(state.init).toBe("Hello World!");
+  it("picks closest getter for computed", () => {
+    const instance = Child.create();
+    expect(instance.value4).toBe("bar");
+  });
 })
 
-it('can initialize a Provider', () => {
-  test(() => {
-    const control = Subject.use();
-    void control.Provider;
-    return control;
+describe("extension", () => {
+  class Subject extends Controller {
+    value = 1;
+    value2 = 2;
+
+    constructor(
+      public init?: string){
+      super();
+    }
+
+    setValueToThree = () => {
+      this.value = 3;
+    }
+  }
+
+  it('initializes from extended Controller', () => {
+    const { state } = test(Subject);
+
+    expect(state.value).toBe(1);
+    expect(state.value2).toBe(2);
+  })
+
+  it('passes arguments to constructor', () => {
+    const { state } = test(() => {
+      return Subject.use("Hello World!")
+    })
+
+    expect(state.init).toBe("Hello World!");
+  })
+
+  it('can initialize a Provider', () => {
+    test(() => {
+      const control = Subject.use();
+      void control.Provider;
+      return control;
+    })
   })
 })
