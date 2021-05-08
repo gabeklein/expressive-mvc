a73fa7e9303c8c21a2a05d619cddb6fff4997fd2
Gabe Klein <gabe@termtm.com>
2021-05-08 01:39:31 -0400

    reorganize common types

--- types/common.d.ts
+++ types/common.d.ts
@@ -1,20 +1,17 @@
-type Callback = () => void;
 type BunchOf<T> = { [key: string]: T };
-type Async = (...args: any[]) => Promise<any>;
+type Recursive<T> = { [K in keyof T]: Recursive<Omit<T, K>> };
+type ReplaceAll<T, R> = { [K in keyof T]: R };
 
 type Select<T> = (arg: T) => any;
-type Recursive<T> = { [K in keyof T]: Recursive<Omit<T, K>> };
 type Selector<T> = (select: Recursive<T>) => void;
 
 type StringsOptional = (string | undefined)[];
-
-type ReplaceAll<T, R> = { [K in keyof T]: R };
-
-type Class = new(...args: any[]) => any;
+type Async = (...args: any[]) => Promise<any>;
+type Class = new (...args: any[]) => any;
 type Expecting<A extends any[]> = new(...args: A) => any;
 type InstanceOf<T> = T extends { prototype: infer U } ? U : never;
 
+type Callback = () => void;
 type EffectCallback<T, A = T> = (this: T, argument: A) => Callback | Promise<any> | void;
-
 type ValueCallback<T, V> = (this: T, value: V, updated: keyof T) => void;
 type UpdateCallback<T, P extends keyof T> = (this: T, value: T[P], changed: P) => void;
