a78c4be6ef2b186777f586606d7c7fd768147004
Gabe Klein <gabe@termtm.com>
2021-05-21 12:38:01 -0600

    tests for built-in lifecycle

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -25,21 +25,6 @@ describe("controller", () => {
 
     expect(state.value).toBe(3);
   })
-  
-  it('will call didCreate method if exists', () => {
-    class Subject extends Controller {
-      constructor(callback: () => void){
-        super();
-        this.didCreate = callback;
-      }
-    }
-
-    function init(this: Subject){
-      expect(this).toStrictEqual(state);
-    }
-
-    const state = Subject.create(init);
-  })
 
   it('will update when a value changes', async () => {
     const state = Subject.create();
--- tests/lifecycle.test.ts
+++ tests/lifecycle.test.ts
@@ -37,3 +37,33 @@ describe("declare method", () => {
     expect(mock).toHaveBeenCalledWith("foo", "bar");
   })
 })
+
+describe("built-in", () => {
+  it('calls `didCreate` when initialized', () => {
+    class Subject extends Controller {
+      didCreate(){
+        mock(this);
+      }
+    }
+
+    const mock = jest.fn();
+    const instance = Subject.create();
+
+    expect(mock).toHaveBeenCalledWith(instance);
+  })
+
+  it('calls `willDestroy` will when killed', () => {
+    class Subject extends Controller {
+      willDestroy(){
+        mock(this);
+      }
+    }
+
+    const mock = jest.fn();
+    const instance = Subject.create();
+
+    instance.destroy();
+
+    expect(mock).toHaveBeenCalledWith(instance);
+  })
+})
