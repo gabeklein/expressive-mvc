35969996ad774008ddcbd93549f7ee076946f005
Gabe Klein <gabe@termtm.com>
2021-06-08 16:45:49 -0600

    combine Model composition tests

--- tests/context.test.tsx
+++ tests/context.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Consumer, Issue, Model, Provider, render, Singleton, tap } from './adapter';
+import { Consumer, Issue, Model, Provider, render } from './adapter';
 
 class Foo extends Model {
   value?: string = undefined;
@@ -149,87 +149,3 @@ describe("Consumer", () => {
     )
   })
 });
-
-describe("Peers", () => {
-  class Foo extends Model {
-    bar = tap(Bar);
-  }
-
-  class Bar extends Model {
-    value = "bar";
-  }
-
-  it("will attach property via tap directive", () => {
-    const Test = () => {
-      const { bar } = Foo.use();
-      expect(bar.value).toBe("bar");
-      return null;
-    }
-
-    render(
-      <Provider of={Bar}>
-        <Test />
-      </Provider>
-    );
-  })
-
-  it("will return undefined if instance not found", () => {
-    const Test = () => {
-      const foo = Foo.use();
-      expect(foo.bar).toBeUndefined();
-      return null;
-    }
-
-    render(<Test />);
-  })
-
-  it("will throw if strict tap is undefined", () => {
-    class Foo extends Model {
-      bar = tap(Bar, true);
-    }
-
-    const issue = Issue.AmbientRequired(Bar.name, Foo.name, "bar");
-    const useStrictFooBar = () => Foo.use().bar;
-
-    const TestComponent = () => {
-      expect(useStrictFooBar).toThrow(issue);
-      return null;
-    }
-
-    render(<TestComponent />);
-  })
-
-  it("will attach a singleton via tap directive", () => {
-    class Foo extends Model {
-      global = tap(Global);
-    }
-
-    class Global extends Singleton {
-      value = "bar";
-    }
-
-    Global.create();
-
-    const Test = () => {
-      const { global } = Foo.use();
-      expect(global.value).toBe("bar");
-      return null;
-    }
-
-    render(<Test />);
-  })
-
-  it("will throw if model is tapped by singleton", () => {
-    class Normal extends Model {}
-    class Global extends Singleton {
-      notPossible = tap(Normal);
-    }
-
-    const attempt = () => Global.create();
-    const issue = Issue.CantAttachGlobal(Global.name, Normal.name);
-
-    expect(attempt).toThrow(issue);
-  })
-
-  it.todo("can access peers sharing same provider");
-})
