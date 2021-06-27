ccdaf78a6deb6ad6f8c54fdf5a9d8f377d2a7887
Gabe Klein <gabe@termtm.com>
2021-06-27 16:36:36 -0600

    add Singleton tests

---
+++ tests/singleton.test.ts
@@ -0,0 +1,46 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { Oops } from "../src/singleton";
+import { Singleton } from "./adapter";
+
+class Global extends Singleton {
+  value = 1;
+}
+
+it("will access values from create global", () => {
+  const hook = renderHook(() => Global.use());
+
+  expect(hook.result.current.value).toBe(1);
+})
+
+it("will find existing instance", () => {
+  const instance = Global.create();
+  const found = Global.find();
+
+  expect(found).toStrictEqual(instance);
+  
+  instance.destroy();
+})
+
+it("will complain if not initialized", () => {
+  const hook = renderHook(() => Global.tap());
+  const expected = Oops.GlobalDoesNotExist(Global.name);
+  
+  expect(() => hook.result.current).toThrowError(expected);
+})
+
+it.skip("will access values from found global", () => {
+  const instance = Global.use();
+  const hook = renderHook(() => Global.get("value"));
+
+  expect(() => hook.result.current).toReturnWith(1);
+  instance.destroy();
+})
+
+it("will complain already exists", () => {
+  const expected = Oops.GlobalExists(Global.name);
+  const instance = Global.create();
+  
+  expect(() => Global.create()).toThrowError(expected);
+
+  instance.destroy();
+})
