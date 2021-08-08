473b1682dde1cfecc0065248d0e91b82ac639865
Gabe Klein <gabe@termtm.com>
2021-08-07 22:17:58 -0400

    refactor lifecycle tests

--- tests/lifecycle.test.ts
+++ tests/lifecycle.test.ts
@@ -51,67 +51,157 @@ describe("lifecycle", () => {
     componentWillUpdate = jest.fn();
   }
 
-  function expectToBeCalled(
-    as: string | symbol | false | number | undefined,
-    methods: jest.Mock<any, any>[]){
-
-    for(const mock of methods)
-      if(as === false)
-        expect(mock).not.toBeCalled();
-      else
-        expect(mock).toBeCalledWith(as);
-  }
-
-  function eventChecker(
-    source: any,
-    keys: string[]){
+  describe("event methods", () => {
+    function expectToBeCalled(
+      as: string | symbol | false | number | undefined,
+      methods: jest.Mock<any, any>[]){
   
-    const capture = jest.fn();
-
-    source.on(keys as any[], capture);
-
-    return (names: string[], expected = true) => {
-      for(const name of names){
-        let check = expect(capture);
-
-        if(expected == false)
-          check = check.not as jest.JestMatchers<any>;
-
-        check.toBeCalledWith(undefined, name)
-      }
+      for(const mock of methods)
+        if(as === false)
+          expect(mock).not.toBeCalled();
+        else
+          expect(mock).toBeCalledWith(as);
     }
-  }
 
-  describe("component", () => {
-    it("will call applicable methods", async () => {
-      const element = renderHook(() => Test.use());
-      const x = element.result.current;
+    describe("use", () => {
+      it("will call component methods", async () => {
+        const element = renderHook(() => Test.use());
+        const x = element.result.current;
+    
+        expectToBeCalled(undefined, [
+          x.willRender, x.componentWillRender,
+          x.willMount, x.componentWillMount,
+          x.didMount, x.componentDidMount
+        ]);
+    
+        expectToBeCalled(false, [
+          x.willUpdate, x.componentWillUpdate,
+          x.willUnmount, x.componentWillUnmount,
+        ]);
+        
+        element.rerender();
+    
+        expectToBeCalled(undefined, [
+          x.willUpdate, x.componentWillUpdate
+        ]);
+    
+        element.unmount();
+    
+        expectToBeCalled(undefined, [
+          x.willUnmount, x.componentWillUnmount
+        ]);
+      })
   
-      expectToBeCalled(undefined, [
-        x.willRender, x.componentWillRender,
-        x.willMount, x.componentWillMount,
-        x.didMount, x.componentDidMount
-      ]);
-  
-      expectToBeCalled(false, [
-        x.willUpdate, x.componentWillUpdate,
-        x.willUnmount, x.componentWillUnmount,
-      ]);
-      
-      element.rerender();
-  
-      expectToBeCalled(undefined, [
-        x.willUpdate, x.componentWillUpdate
-      ]);
-  
-      element.unmount();
-  
-      expectToBeCalled(undefined, [
-        x.willUnmount, x.componentWillUnmount
-      ]);
+      it("will not call element methods", async () => {
+        const element = renderHook(() => Test.use());
+        const test = element.result.current;
+    
+        expectToBeCalled(false, [
+          test.elementWillRender,
+          test.elementWillMount,
+          test.elementDidMount
+        ]);
+      })
     })
 
-    it("will emit applicable events", async () => {
+    describe("tag", () => {
+      it("will call element methods", async () => {
+        const x = Test.create();
+        const identifier = "foobar";
+    
+        const element = renderHook(() => x.tag(identifier));
+    
+        expectToBeCalled(identifier, [
+          x.willRender, x.elementWillRender,
+          x.willMount, x.elementWillMount,
+          x.didMount, x.elementDidMount
+        ]);
+    
+        expectToBeCalled(false, [
+          x.willUpdate, x.elementWillUpdate,
+          x.willUnmount, x.elementWillUnmount,
+        ]);
+        
+        element.rerender();
+    
+        expectToBeCalled(identifier, [
+          x.willUpdate, x.elementWillUpdate
+        ]);
+    
+        element.unmount();
+    
+        expectToBeCalled(identifier, [
+          x.willUnmount, x.elementWillUnmount
+        ]);
+      })
+  
+      it("will not call component methods", async () => {
+        const test = Test.create();
+    
+        renderHook(() => test.tag("foobar"));
+    
+        expectToBeCalled(false, [
+          test.componentWillRender,
+          test.componentWillMount,
+          test.componentDidMount
+        ]);
+      })
+
+      it("will pass a value to method", async () => {
+        const test = Test.create();
+        const identifier = Symbol("foobar");
+    
+        renderHook(() => test.tag(identifier));
+    
+        expectToBeCalled(identifier, [ test.willRender ]);
+      })
+  
+      it("will pass a value from factory", async () => {
+        const test = Test.create();
+        const identifier = Symbol("foobar");
+        const factory = (on: Test) => {
+          expect(on).toBe(test);
+          return identifier;
+        }
+    
+        renderHook(() => test.tag(factory));
+    
+        expectToBeCalled(identifier, [ test.willRender ]);
+      })
+  
+      it("will pass 0 if no value provided", () => {
+        const test = Test.create();
+        renderHook(() => test.tag());
+  
+        expectToBeCalled(0, [ test.willRender ]);
+      })
+    })
+  })
+
+  describe("event listeners", () => {
+    function eventChecker(
+      source: any,
+      keys: string[]){
+    
+      const capture = jest.fn();
+  
+      source.on(keys as any[], capture);
+  
+      return (names: string[], expected = true) => {
+        for(const name of names){
+          let check = expect(capture);
+  
+          if(expected == false)
+            check = check.not as jest.JestMatchers<any>;
+  
+          check.toBeCalledWith(undefined, name)
+        }
+      }
+    }
+
+    it("will emit component events", async () => {
+      let expect!: (names: string[], expected?: boolean) => void;
+
       class LocalTest extends Test {
         constructor(){
           super();
@@ -125,8 +215,6 @@ describe("lifecycle", () => {
           ])
         }
       }
-
-      let expect!: (names: string[], expected?: boolean) => void;
       const element = renderHook(() => LocalTest.use());
       const x = element.result.current;
 
@@ -157,53 +245,8 @@ describe("lifecycle", () => {
         "willUnmount", "componentWillUnmount"
       ]);
     })
-
-    it("will not call non-applicable", async () => {
-      const element = renderHook(() => Test.use());
-      const test = element.result.current;
-  
-      expectToBeCalled(false, [
-        test.elementWillRender,
-        test.elementWillMount,
-        test.elementDidMount
-      ]);
-    })
-
-    it.todo("will call methods in order")
-  })
-
-  describe("element", () => {
-    it("will call applicable methods", async () => {
-      const x = Test.create();
-      const identifier = "foobar";
-  
-      const element = renderHook(() => x.tag(identifier));
-  
-      expectToBeCalled(identifier, [
-        x.willRender, x.elementWillRender,
-        x.willMount, x.elementWillMount,
-        x.didMount, x.elementDidMount
-      ]);
-  
-      expectToBeCalled(false, [
-        x.willUpdate, x.elementWillUpdate,
-        x.willUnmount, x.elementWillUnmount,
-      ]);
-      
-      element.rerender();
-  
-      expectToBeCalled(identifier, [
-        x.willUpdate, x.elementWillUpdate
-      ]);
-  
-      element.unmount();
-  
-      expectToBeCalled(identifier, [
-        x.willUnmount, x.elementWillUnmount
-      ]);
-    })
-
-    it("will emit applicable events", async () => {
+    
+    it("will emit element events", async () => {
       const x = Test.create();
 
       const expect = eventChecker(x, [
@@ -241,47 +284,6 @@ describe("lifecycle", () => {
       expect([
         "willUnmount", "elementWillUnmount"
       ]);
-    })
-
-    it("will not call non-applicable", async () => {
-      const test = Test.create();
-  
-      renderHook(() => test.tag("foobar"));
-  
-      expectToBeCalled(false, [
-        test.componentWillRender,
-        test.componentWillMount,
-        test.componentDidMount
-      ]);
-    })
-
-    it("will pass a symbol also", async () => {
-      const test = Test.create();
-      const identifier = Symbol("foobar");
-  
-      renderHook(() => test.tag(identifier));
-  
-      expectToBeCalled(identifier, [ test.willRender ]);
-    })
-
-    it("will pass result from factory", async () => {
-      const test = Test.create();
-      const identifier = Symbol("foobar");
-      const factory = (on: Test) => {
-        expect(on).toBe(test);
-        return identifier;
-      }
-  
-      renderHook(() => test.tag(factory));
-  
-      expectToBeCalled(identifier, [ test.willRender ]);
-    })
-
-    it("will default to 0-key if not provided", () => {
-      const test = Test.create();
-      renderHook(() => test.tag());
-
-      expectToBeCalled(0, [ test.willRender ]);
     })
   })
 })
