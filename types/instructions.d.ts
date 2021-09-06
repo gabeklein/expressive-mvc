5bf2a17119dafd901f937ccc01b637a2b45c20c0
Gabe Klein <gabe@termtm.com>
2021-09-06 00:57:48 -0600

    fix indentation

--- types/instructions.d.ts
+++ types/instructions.d.ts
@@ -5,14 +5,11 @@ import { Async, Class, EffectCallback, InstanceOf } from './types';
  * Run instruction as controller sets itself up.
  * This will specialize the behavior of a given property.
  **/
-export function set <T = any> (
-  instruction: Model.Instruction<T>,
-  name?: string
-): T;
+export function set <T = any> (instruction: Model.Instruction<T>, name?: string): T;
 
 /**
  * Creates a new child-instance of specified controller.
- * 
+ *
  * @param Peer - Type of Model to create and apply to host property.
  * @param callback - Fired after controller is created and ready for use.
  */
@@ -20,7 +17,7 @@ export function use <T extends typeof Model> (Peer: T, callback: (i?: InstanceOf
 
 /**
  * Creates a new child-instance of specified controller.
- * 
+ *
  * @param Peer - Type of Model to create and apply to host property.
  * @param required - Property should be non-nullable, will throw if value is set to undefined.
  */
@@ -29,7 +26,7 @@ export function use <T extends typeof Model> (Peer: T, required?: true): Instanc
 
 /**
  * Creates a new child-instance of specified controller.
- * 
+ *
  * @param factory - Generate model for use.
  * @param callback - Fired after controller is created and ready for use.
  */
@@ -37,12 +34,12 @@ export function use <T extends Model | undefined> (factory: () => T, callback?: 
 
 /**
  * Create a child-instance relationship with provided model.
- * 
+ *
  * Note: If `peer` is not already initialized before parent is
  * (created with `new` as opposed to create method), that model will
  * attach this via `parent()` instruction. It will not howerver, if
  * already active.
- * 
+ *
  * @param factory - Instance of model to attach.
  * @param callback - Fired after controller is attached.
  */
@@ -55,92 +52,92 @@ export function use <T extends Model> (): T | undefined;
 
 /**
  * Generate a child controller from specified object. Object's values are be trackable, as would be for a full-model.
- * 
+ *
  * Note: Child will *not* be same object as one provided.
  */
 export function use <T extends {}> (object: T): T;
 
 /**
  * Generic instruction used by `use()` and `tap()` for recursive subscription.
- * 
+ *
  * @param from - Instruction body is run upon parent create. Return function to fetch current value of field.
  */
 export function child <T extends Model> (from: (this: Model.Controller, key: string) => () => T | undefined): T;
  
- /**
-  * Fetches and assigns the controller which spawned this host.
-  * When parent assigns an instance via `use()` directive, it
-  * will be made available to child upon this request.
-  * 
-  * @param Expects - Type of controller compatible with this class. 
-  * @param required - Throw if controller is created independantly.
-  */
- export function parent <T extends typeof Model> (Expects: T, required: true): InstanceOf<T>;
- export function parent <T extends typeof Model> (Expects: T, required?: false): InstanceOf<T> | undefined;
+/**
+ * Fetches and assigns the controller which spawned this host.
+ * When parent assigns an instance via `use()` directive, it
+ * will be made available to child upon this request.
+ *
+ * @param Expects - Type of controller compatible with this class. 
+ * @param required - Throw if controller is created independantly.
+ */
+export function parent <T extends typeof Model> (Expects: T, required: true): InstanceOf<T>;
+export function parent <T extends typeof Model> (Expects: T, required?: false): InstanceOf<T> | undefined;
  
- /**
-  * Find and attach most applicable instance of Model via context.
-  * 
-  * Expects a `<Provider>` of target controller to exist. 
-  * Host controller will search element-hierarchy relative to where it spawned.
-  * 
-  * @param Type - Type of controller to attach to property. 
-  * @param required - Throw if instance of Type cannot be found.
-  */
- export function tap <T extends Class> (Type: T, required?: boolean): InstanceOf<T>;
+/**
+ * Find and attach most applicable instance of Model via context.
+ *
+ * Expects a `<Provider>` of target controller to exist. 
+ * Host controller will search element-hierarchy relative to where it spawned.
+ *
+ * @param Type - Type of controller to attach to property. 
+ * @param required - Throw if instance of Type cannot be found.
+ */
+export function tap <T extends Class> (Type: T, required?: boolean): InstanceOf<T>;
  
- /**
-  * Sets property to synchronously call effect upon update.
-  * 
-  * @param starting - Beginning value of host property.
-  * @param callback - Effect-callback fired upon set of host property.
-  */
- export function on <T = any> (initial: undefined, callback: EffectCallback<T>): T | undefined;
- export function on <T = any> (initial: T, callback: EffectCallback<T>): T;
+/**
+ * Sets property to synchronously call effect upon update.
+ *
+ * @param starting - Beginning value of host property.
+ * @param callback - Effect-callback fired upon set of host property.
+ */
+export function on <T = any> (initial: undefined, callback: EffectCallback<T>): T | undefined;
+export function on <T = any> (initial: T, callback: EffectCallback<T>): T;
 
- /**
-  * Creates a ref-compatible property for use with components.
-  * Will persist value, and updates to this are made part of controller event-stream.
-  * 
-  * *Output is simultaneously a ref-function and ref-object, use as needed.*
-  * 
-  * @param callback - Optional callback to synchronously fire when reference is first set or does update.
-  */
- export function ref <T = HTMLElement> (callback?: EffectCallback<T>): Model.Ref<T>;
+/**
+ * Creates a ref-compatible property for use with components.
+ * Will persist value, and updates to this are made part of controller event-stream.
+ *
+ * *Output is simultaneously a ref-function and ref-object, use as needed.*
+ *
+ * @param callback - Optional callback to synchronously fire when reference is first set or does update.
+ */
+export function ref <T = HTMLElement> (callback?: EffectCallback<T>): Model.Ref<T>;
 
- /**
-  * Sets an exotic method with managed ready-state. Property accepts an async function.
-  * 
-  * When an act-method is invoked, its `active` property to true for duration of call.
-  * This is emitted as an update to property, both when called and after returns (or throws).
-  * 
-  * **Note:** Subsequent calls will immediately throw if one is still pending.
-  * 
-  * @param action - Action to fire when resulting property is invoked.
-  */
- export function act <T extends Async>(action: T): T & { active: boolean };
+/**
+ * Sets an exotic method with managed ready-state. Property accepts an async function.
+ *
+ * When an act-method is invoked, its `active` property to true for duration of call.
+ * This is emitted as an update to property, both when called and after returns (or throws).
+ *
+ * **Note:** Subsequent calls will immediately throw if one is still pending.
+ *
+ * @param action - Action to fire when resulting property is invoked.
+ */
+export function act <T extends Async> (action: T): T & { active: boolean };
  
- /**
-  * Implement a computed value; output will be generated by provided function.
-  * 
-  * *Alternative to using native getters; works exactly the same!*
-  * 
-  * @param fn - Factory function to generate value and subscribe dependancies.
-  */
- export function from <T, R>(fn: (this: T, on: T) => R): R;
+/**
+ * Implement a computed value; output will be generated by provided function.
+ *
+ * *Alternative to using native getters; works exactly the same!*
+ *
+ * @param fn - Factory function to generate value and subscribe dependancies.
+ */
+export function from <T, R> (fn: (this: T, on: T) => R): R;
 
- /**
-  * Memoized value. Computes and stores a value returned by provided factory. 
-  * Equivalent to a getter, sans the automatic updates.
-  * 
-  * @param compute - Factory for memoized value.
-  * @param lazy - Wait until accessed to introduce value.
-  */
- export function memo <T> (compute: () => T, lazy?: boolean): T;
+/**
+ * Memoized value. Computes and stores a value returned by provided factory. 
+ * Equivalent to a getter, sans the automatic updates.
+ *
+ * @param compute - Factory for memoized value.
+ * @param lazy - Wait until accessed to introduce value.
+ */
+export function memo <T> (compute: () => T, lazy?: boolean): T;
  
- /**
-  * Flag property as not to be tracked. Useful if changes often with no real-time impact.
-  * 
-  * @param value - starting value of property.
-  */
- export function lazy <T> (value?: T): T;
+/**
+ * Flag property as not to be tracked. Useful if changes often with no real-time impact.
+ *
+ * @param value - starting value of property.
+ */
+export function lazy <T> (value?: T): T;
