4bc1dff446380edf5c3ddb4e3350f7664ce1b22e
Gabe Klein <gabe@expressive.dev>
2021-11-12 19:58:14 -0500

    fix issue import in test

--- tests/peers.test.tsx
+++ tests/peers.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Oops } from '../src/Peer';
+import { Oops } from '../src/peer';
 import { Consumer, Model, Provider, render, Singleton, subscribeTo, tap } from './adapter';
 
 describe("tap instruction", () => {
