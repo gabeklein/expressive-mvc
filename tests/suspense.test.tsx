91e90735f4277bfe43afcef6e172996cc2e36eca
Gabe Klein <gabe@expressive.dev>
2021-11-20 15:06:26 -0500

    massively improve suspense tests

--- tests/suspense.test.tsx
+++ tests/suspense.test.tsx
@@ -2,49 +2,121 @@ import React, { Suspense } from 'react';
 
 import { Model, render, suspend } from './adapter';
 
+function manageAsync<T = void>(){
+  let trigger!: (value: T) => void;
+
+  const pending = new Promise<T>(resolve => {
+    trigger = resolve;
+  })
+
+  return { pending, trigger }
+}
+
+function scenario(){
+  let tryFunction!: () => void;
+  let didSuspend = false;
+  let didRender = false;
+  let didThrow: Error | undefined;
+
+  const Waiting = () => {
+    didSuspend = true;
+
+    return <div>Waiting...</div>;
+  }
+
+  const Component = () => {
+    try {
+      tryFunction();
+    }
+    catch(err: any){
+      // let suspense do it's thing
+      if(err instanceof Promise)
+        throw err;
+
+      // log and supress otherwise
+      didThrow = err;
+      return null;
+    }
+
+    didRender = true;
+
+    return <div>Content!</div>;
+  }
+
+  return {
+    renderHook(fn: () => void){
+      tryFunction = fn;
+  
+      render(
+        <Suspense fallback={<Waiting/>}>
+          <Component />
+        </Suspense>
+      )
+    },
+    assertDidRender(yes: boolean){
+      expect(didRender).toBe(yes);
+    },
+    assertDidSuspend(yes: boolean){
+      expect(didSuspend).toBe(yes);
+    },
+    assertDidThrow(error: Error){
+      expect(didThrow).toBe(error);
+    }
+  }
+}
+
 describe("suspense", () => {
   it('will auto-suspend if willRender is instruction', async () => {
+    const { trigger, pending } = manageAsync();
+
     class Test extends Model {
-      endSuspense!: () => void;
-
-      willRender = suspend(() => (
-        new Promise(res => { this.endSuspense = res })
-      ))
+      willRender = suspend(() => pending);
     }
 
-    const test = Test.create();
-  
-    let waiting = false;
-    let rendered = false;
-  
-    const Waiting = () => {
-      waiting = true;
+    const test = scenario();
+    const instance = Test.create();
 
-      return <div>Waiting...</div>;
+    test.assertDidSuspend(false);
+
+    test.renderHook(() => {
+      instance.tap();
+    })
+  
+    test.assertDidSuspend(true);
+    test.assertDidRender(false);
+
+    trigger();
+    await instance.update();
+
+    test.assertDidRender(true);
+  })
+  
+  it('will refresh and throw if async rejects', async () => {
+    const { trigger, pending } = manageAsync();
+    const error = new Error("some foobar went down");
+
+    class Test extends Model {
+      willRender = suspend(async () => {
+        await pending;
+        throw error;
+      })
     }
-  
-    const Component = () => {
-      void test.tap();
-      rendered = true;
-  
-      return <div>Content!</div>;
-    }
 
-    expect(waiting).toBe(false);
+    const test = scenario();
+    const instance = Test.create();
+
+    test.assertDidSuspend(false);
+
+    test.renderHook(() => {
+      instance.tap();
+    })
   
-    render(
-      <Suspense fallback={<Waiting/>}>
-        <Component />
-      </Suspense>
-    )
-  
-    expect(waiting).toBe(true);
-    expect(rendered).toBe(false);
+    test.assertDidSuspend(true);
+    test.assertDidRender(false);
 
-    test.endSuspense();
+    trigger();
+    await instance.update();
 
-    await test.update();
-
-    expect(rendered).toBe(true);
+    test.assertDidThrow(error);
   })
 })
