991dd22bcfe9af7b28be4615433c3893a85126c7
Gabe Klein <gabe@termtm.com>
2022-04-27 13:29:27 -0400

    Implement Issue namespace

--- src/issues.ts
+++ src/issues.ts
@@ -1,15 +1,17 @@
-type MessageVar = {} | string | number | boolean | null;
-type ExpectVars<T> = T extends (...args: infer T) => any ? T : never;
-
-type Messages = {
-  [named: string]: (...args: MessageVar[]) => string;
+declare namespace Issue {
+  export type Variable = {} | string | number | boolean | null;
+  export type Variables<T> = T extends (...args: infer T) => any ? T : never;
+  
+  export type Messages = {
+    [named: string]: (...args: Variable[]) => string;
+  }
+  
+  export type Output<M extends Messages> = {
+    readonly [P in keyof M]: (...args: Variables<M[P]>) => Issue;
+  }
 }
 
-type Issues<M extends Messages> = {
-  readonly [P in keyof M]: (...args: ExpectVars<M[P]>) => Issue;
-}
-
-class Issue extends Error {
+export class Issue extends Error {
   // drop first line (error factory) in stack trace.
   stack = this.stack.replace(/\n.+/, "") as string;
 
@@ -18,12 +20,12 @@ class Issue extends Error {
   }
 }
 
-export function issues<M extends Messages>(register: M){
+export function issues<M extends Issue.Messages>(register: M){
   const Library = {} as any;
   
   for(const name in register)
-    Library[name] = (...args: MessageVar[]) => 
+    Library[name] = (...args: Issue.Variable[]) => 
       new Issue(register[name].apply(null, args));
 
-  return Library as Issues<M>;
+  return Library as Issue.Output<M>;
 }
