fa4421916ab36051f3c1d5f5948f6ce6558a90e5
Gabe Klein <gabe@termtm.com>
2022-04-27 13:36:00 -0400

    generate public types on build

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,37 @@
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
 
-export default  {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'lib/index.js',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: 'lib/index.esm.js',
-      format: 'esm',
-      sourcemap: true
-    }
-  ],
-  external: [ 'react' ],
-  plugins: [
-    typescript(),
-    commonjs()
-  ]
-}
+export default  [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/index.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: 'lib/index.esm.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    external: [ 'react' ],
+    plugins: [
+      typescript(),
+      commonjs()
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [{
+      file: 'lib/index.d.ts',
+      format: 'es'
+    }],
+    plugins: [
+      dts()
+    ]
+  }
+]
