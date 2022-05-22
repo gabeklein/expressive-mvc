e90bbd54c1dfa7e934d8be1c4cae5821c48a1154
Gabe Klein <gabe@termtm.com>
2022-05-22 15:32:40 -0400

    add timeout utility

--- tests/adapter.ts
+++ tests/adapter.ts
@@ -110,3 +110,20 @@ export function mockSuspense(){
     }
   }
 }
+
+export function timeout(ms = 0){
+  return new Promise(res => setTimeout(res, ms));
+}
+
+export async function ensure<T>(suspense: () => T){
+  try {
+    suspense();
+    throw new Error("Expected suspense.");
+  }
+  catch(err){
+    if(err instanceof Promise)
+      return err as Promise<T>;
+    else
+      throw err;
+  }
+}
