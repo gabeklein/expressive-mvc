788f4108b4c04a83473538ba7664a9262f5fc303
Gabe Klein <gabe@termtm.com>
2022-05-29 11:49:31 -0400

    improve coverage for array instruction

--- src/instruction/array.test.ts
+++ src/instruction/array.test.ts
@@ -23,6 +23,47 @@ it("will update on push to array", async () => {
   expect(mock).toBeCalledWith(["hello"]);
 })
 
+it("will update on unshift to array", async () => {
+  const test = Test.create();
+  const mock = jest.fn();
+  
+  test.effect(state => {
+    mock([ ...state.values ]);
+  })
+
+  expect(mock).toBeCalledWith([]);
+
+  test.values.unshift("hello");
+
+  await test.update(true);
+
+  expect(mock).toBeCalledTimes(2);
+  expect(mock).toBeCalledWith(["hello"]);
+})
+
+it("will update on splice to array", async () => {
+  const test = Test.create();
+  const mock = jest.fn();
+  
+  test.effect(state => {
+    mock([ ...state.values ]);
+  })
+
+  expect(mock).toBeCalledWith([]);
+
+  test.values.splice(0, 0, "hello");
+
+  await test.update(true);
+
+  test.values.splice(0, 1, "goodbye");
+
+  await test.update(true);
+
+  expect(mock).toBeCalledTimes(3);
+  expect(mock).toBeCalledWith(["hello"]);
+  expect(mock).toBeCalledWith(["goodbye"]);
+})
+
 it.todo("will subscribe to a specific slice");
 it.todo("will update only spliced range");
 it.todo("will update all after spliced range");
