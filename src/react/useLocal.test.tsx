92bbf0cb5f52db1b1936e3595f0323e9304481b2
Gabe Klein <gabe@termtm.com>
2022-06-01 22:30:46 -0400

    use actual hook in tests

--- src/react/useLocal.test.tsx
+++ src/react/useLocal.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from '../../tests/adapter';
 import { MVC } from './mvc';
 import { Provider } from './provider';
-import { Oops } from './useLocal';
+import { Oops, useLocal } from './useLocal';
 
 describe("get", () => {
   class Test extends MVC {
@@ -12,7 +12,7 @@ describe("get", () => {
 
   it("will get instance of model", () => {
     const Hook = () => {
-      const value = Test.get("value");
+      const value = useLocal(Test, "value");
       expect(value).toBe("foo")
       return null;
     }
@@ -26,7 +26,7 @@ describe("get", () => {
 
   it("will fail if not found", () => {
     const Hook = () => {
-      Test.get();
+      useLocal(Test);
       return null;
     }
 
