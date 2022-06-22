c96f68201b6d85923bd8dd48a9de90c4660ca39e
Gabe Klein <gabe@termtm.com>
2022-06-22 17:41:06 -0400

    deprecate object-ref instruction

--- src/instruction/ref.test.ts
+++ src/instruction/ref.test.ts
@@ -1,145 +1,101 @@
 import { Model, ref } from '..';
 
-describe("single", () => {
-  class Subject extends Model {
-    didTrigger = jest.fn();
 
-    ref1 = ref<string>();
+class Subject extends Model {
+  didTrigger = jest.fn();
 
-    ref2 = ref<symbol>(value => {
-      this.didTrigger(value);
-    })
+  ref1 = ref<string>();
 
-    ref3 = ref<number>(() => {
-      return () => {
-        this.didTrigger(true);
-      }
-    })
-  }
-
-  it('will fetch value from ref-object', async () => {
-    const state = Subject.create();
-
-    state.ref1.current = "foobar";
-
-    await state.update(true);
-    expect(state.ref1.current).toBe("foobar");
+  ref2 = ref<symbol>(value => {
+    this.didTrigger(value);
   })
 
-  it('will watch "current" of property', async () => {
-    const state = Subject.create();
-    const callback = jest.fn()
-
-    state.once("ref1", callback);
-    state.ref1.current = "foobar";
-
-    await state.update(true);
-    expect(callback).toBeCalledWith("foobar", "ref1");
-  })
-
-  it('will update "current" when property invoked', async () => {
-    const state = Subject.create();
-    const callback = jest.fn()
-
-    state.once("ref1", callback);
-    state.ref1("foobar");
-
-    await state.update(true);
-    expect(callback).toBeCalledWith("foobar", "ref1");
-  })
-
-  it('will invoke callback if exists', async () => {
-    const state = Subject.create();
-    const targetValue = Symbol("inserted object");
-    const callback = jest.fn();
-
-    expect(state.didTrigger).not.toBeCalled();
-    state.once("ref2", callback);
-    state.ref2.current = targetValue;
-    expect(state.didTrigger).toBeCalledWith(targetValue);
-
-    await state.update(true);
-    expect(callback).toBeCalledWith(targetValue, "ref2");
-  })
-
-  it('will invoke return-callback on overwrite', async () => {
-    const state = Subject.create();
-
-    state.ref3.current = 1;
-
-    await state.update();
-    expect(state.didTrigger).not.toBeCalled();
-    state.ref3.current = 2;
-
-    await state.update();
-    expect(state.didTrigger).toBeCalledWith(true);
-  })
-
-  it('will export value of ref-properties', () => {
-    const test = Subject.create();
-    const values = {
-      ref1: "foobar",
-      ref2: Symbol("foobar"),
-      ref3: 69420
+  ref3 = ref<number>(() => {
+    return () => {
+      this.didTrigger(true);
     }
-
-    test.ref1(values.ref1);
-    test.ref2(values.ref2);
-    test.ref3(values.ref3);
-
-    const state = test.export();
-
-    expect(state).toMatchObject(values);
   })
+}
 
-  it('will be accessible from a proxy', () => {
-    const test = Subject.create();
+it('will fetch value from ref-object', async () => {
+  const state = Subject.create();
 
-    test.effect(state => {
-      expect(state.ref1).not.toBeUndefined();
-    })
-  })
+  state.ref1.current = "foobar";
+
+  await state.update(true);
+  expect(state.ref1.current).toBe("foobar");
 })
 
-describe("proxy", () => {
-  class Subject extends Model {
-    foo = "foo";
-    bar = "bar";
+it('will watch "current" of property', async () => {
+  const state = Subject.create();
+  const callback = jest.fn()
 
-    refs = ref(this);
+  state.once("ref1", callback);
+  state.ref1.current = "foobar";
+
+  await state.update(true);
+  expect(callback).toBeCalledWith("foobar", "ref1");
+})
+
+it('will update "current" when property invoked', async () => {
+  const state = Subject.create();
+  const callback = jest.fn()
+
+  state.once("ref1", callback);
+  state.ref1("foobar");
+
+  await state.update(true);
+  expect(callback).toBeCalledWith("foobar", "ref1");
+})
+
+it('will invoke callback if exists', async () => {
+  const state = Subject.create();
+  const targetValue = Symbol("inserted object");
+  const callback = jest.fn();
+
+  expect(state.didTrigger).not.toBeCalled();
+  state.once("ref2", callback);
+  state.ref2.current = targetValue;
+  expect(state.didTrigger).toBeCalledWith(targetValue);
+
+  await state.update(true);
+  expect(callback).toBeCalledWith(targetValue, "ref2");
+})
+
+it('will invoke return-callback on overwrite', async () => {
+  const state = Subject.create();
+
+  state.ref3.current = 1;
+
+  await state.update();
+  expect(state.didTrigger).not.toBeCalled();
+  state.ref3.current = 2;
+
+  await state.update();
+  expect(state.didTrigger).toBeCalledWith(true);
+})
+
+it('will export value of ref-properties', () => {
+  const test = Subject.create();
+  const values = {
+    ref1: "foobar",
+    ref2: Symbol("foobar"),
+    ref3: 69420
   }
 
-  it("will match properties", () => {
-    const test = Subject.create();
+  test.ref1(values.ref1);
+  test.ref2(values.ref2);
+  test.ref3(values.ref3);
 
-    for(const key in test)
-      expect(test.refs).toHaveProperty(key);
-  })
+  const state = test.export();
 
-  it("will match values via current", () => {
-    const test = Subject.create();
-    const { refs } = test;
+  expect(state).toMatchObject(values);
+})
 
-    for(const key in test){
-      const value = (test as any)[key];
-      const { current } = (refs as any)[key];
+it('will be accessible from a proxy', () => {
+  const test = Subject.create();
 
-      expect(current).toBe(value);
-    }
-  })
-
-  it("will update values", async () => {
-    const test = Subject.create();
-
-    expect(test.foo).toBe("foo");
-    expect(test.bar).toBe("bar");
-
-    test.refs.foo("bar");
-    test.refs.bar("foo");
-
-    await test.update(true);
-
-    expect(test.foo).toBe("bar");
-    expect(test.bar).toBe("foo");
+  test.effect(state => {
+    expect(state.ref1).not.toBeUndefined();
   })
 })
--- src/instruction/ref.ts
+++ src/instruction/ref.ts
@@ -1,7 +1,5 @@
-import { Controller } from '../controller';
 import { Model } from '../model';
-import { ensure } from '../stateful';
-import { createValueEffect, defineLazy, defineProperty } from '../util';
+import { createValueEffect, defineProperty } from '../util';
 import { apply } from './apply';
 
 declare namespace ref {
@@ -18,16 +16,6 @@ declare namespace ref {
 }
 
 /**
- * Creates an object with references to all managed values.
- * Each property will set value in state when invoked.
- *
- * *Properties are simultaneously a ref-function and ref-object, use as needed.*
- *
- * @param target - Source model from which to reference values.
- */
-function ref <T extends Model> (target: T): ref.Proxy<T>;
-
-/**
  * Creates a ref-compatible property.
  * Will persist value, and updates to this are made part of controller event-stream.
  *
@@ -38,39 +26,24 @@ function ref <T extends Model> (target: T): ref.Proxy<T>;
 function ref <T = HTMLElement> (callback?: ref.Callback<T>): ref.Object<T>;
 function ref <T, S> (callback?: ref.Callback<T, S>): ref.Object<T>;
 
-function ref<T>(arg?: ref.Callback<T> | Model){
+function ref<T>(callback?: ref.Callback<T>){
   return apply(
     function ref(key){
-      let value: ref.Object | ref.Proxy<any> = {};
+      const value = this.ref(key, 
+        callback && createValueEffect(callback)  
+      );
+    
+      defineProperty(value, "current", {
+        set: value,
+        get: () => this.state.get(key)
+      })
 
-      if(typeof arg == "object"){
-        const source = ensure(arg);
-
-        for(const key of source.state.keys())
-          defineLazy(value, key, createRef.bind(source, key));
-      }
-      else 
-        value = createRef.call(this, key, arg);
-
-      return { value, explicit: true };
+      return {
+        value,
+        explicit: true
+      };
     }
   )
 }
 
 export { ref }
-
-function createRef(
-  this: Controller,
-  key: string,
-  cb?: ref.Callback<any>){
-
-  const refObjectFunction =
-    this.ref(key, cb && createValueEffect(cb));
-
-  defineProperty(refObjectFunction, "current", {
-    set: refObjectFunction,
-    get: () => this.state.get(key)
-  })
-
-  return refObjectFunction as ref.Object;
-}
