6df77c25c9fc60b0c39ac9c6ece316ea4d710c7b
Gabe Klein <gabe@termtm.com>
2022-07-06 16:12:07 -0400

    apply keyed-instruction tests to both Set and Map

--- src/instruction/use/keyed.test.ts
+++ src/instruction/use/keyed.test.ts
@@ -2,167 +2,229 @@ import { Model } from '../..';
 import { from } from '../from';
 import { use } from './use';
 
-describe("set", () => {
+describe("Map", () => {
   class Test extends Model {
-    values = use(new Set());
+    map = use(new Map());
   }
-  
+
   it("will update on set", async () => {
     const test = Test.create();
     const mock = jest.fn();
+
+    test.map.set("foo", "foo");
   
-    test.effect(($) => {
-      mock($.values.has("hello"));
-    })
-  
-    expect(mock).toBeCalledWith(false);
-  
-    test.values.add("hello");
-    await test.update(true);
-  
-    expect(mock).toBeCalledWith(true);
-    test.destroy();
-  })
-  
-  it("will squash simultaneous updates", async () => {
-    const test = Test.create();
-    const mock = jest.fn((state: Test) => {
-      state.values.has("foo");
-      state.values.has("bar");
+    test.effect($ => {
+      mock($.map.get("foo"));
     });
+
+    expect(mock).toBeCalledWith("foo");
   
-    test.effect(mock);
-  
-    test.values.add("foo");
-    test.values.add("bar");
+    test.map.set("foo", "bar");
     await test.update(true);
   
     expect(mock).toBeCalledTimes(2);
+    expect(mock).toBeCalledWith("bar");
   })
-  
-  it("will distrobute simultaneous updates", async () => {
-    const test = Test.create();
-    const mock1 = jest.fn(($: Test) => void $.values.has(1));
-    const mock2 = jest.fn(($: Test) => void $.values.has(2));
-    const mock3 = jest.fn(($: Test) => void $.values.has(3));
-  
-    test.effect(mock1);
-    test.effect(mock2);
-    test.effect(mock3);
-  
-    test.values.add(1);
-    test.values.add(2);
-    test.values.add(3);
-  
-    await test.update();
-  
-    expect(mock1).toBeCalledTimes(2);
-    expect(mock2).toBeCalledTimes(2);
-    expect(mock3).toBeCalledTimes(2);
-  })
-  
-  it("will allow normal methods outside proxy", () => {
-    const { values } = Test.create();
-  
-    values.add("foo");
-    expect(values.has("foo")).toBe(true);
-  })
-  
-  it("will update on delete", async () => {
-    const test = Test.create();
-    const mock = jest.fn((state: Test) => {
-      state.values.has("foo");
-    });
-  
-    test.effect(mock);
-  
-    test.values.add("foo");
-    await test.update(true);
-  
-    test.values.delete("foo");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(3);
-  })
-  
-  it("will not update on unwatched key", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.values.has("hello"));
+})
+
+for(const T of [Map, Set])
+  describe(T, () => {
+    class Test extends Model {
+      values = use(T === Map
+        ? new Map() : new Set()
+      );
+
+      size = from(this, $ => $.values.size);
+
+      insert = (key: any) => {
+        if("set" in this.values)
+          this.values.set(key, key);
+        else
+          this.values.add(key);
+      }
+    }
+    
+    it("will update on add", async () => {
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect(($) => {
+        mock($.values.has("foo"));
+      })
+    
+      expect(mock).toBeCalledWith(false);
+    
+      test.insert("foo");
+      await test.update(true);
+    
+      expect(mock).toBeCalledWith(true);
+      test.destroy();
     })
+    
+    it("will not update for unwatched", async () => {
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect($ => {
+        mock($.values.has("foo"));
+      })
+    
+      expect(mock).toBeCalledTimes(1);
   
-    expect(mock).toBeCalledTimes(1);
-  
-    test.values.add("foo");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(1);
-  })
-  
-  it("will update for any where iterated", async () => {
-    const test = Test.create();
-  
-    const mockIterator = jest.fn(({ values }: Test) => void [...values]);
-    const mockValues = jest.fn(({ values }: Test) => void values.values());
-    const mockKeys = jest.fn(({ values }: Test) => void values.keys());
-    const mockEntries = jest.fn(({ values }: Test) => void values.entries());
-  
-    test.effect(mockIterator);
-    test.effect(mockEntries);
-    test.effect(mockValues);
-    test.effect(mockKeys);
-  
-    test.values.add("foo");
-    await test.update(true);
-  
-    expect(mockIterator).toBeCalledTimes(2);
-    expect(mockEntries).toBeCalledTimes(2);
-    expect(mockValues).toBeCalledTimes(2);
-    expect(mockKeys).toBeCalledTimes(2);
-  })
-  
-  it("will update on clear", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.values.has("hello"));
+      test.insert("bar");
+      await test.update(true);
+    
+      expect(mock).toBeCalledTimes(1);
     })
-  
-    test.values.clear();
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(2);
-  })
-  
-  it("will update any key on replacement", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.values.has("hello"));
+    
+    it("will update on delete", async () => {
+      const test = Test.create();
+      const mock = jest.fn((state: Test) => {
+        state.values.has("foo");
+      });
+    
+      test.effect(mock);
+    
+      test.insert("foo");
+      await test.update(true);
+    
+      test.values.delete("foo");
+      await test.update(true);
+    
+      expect(mock).toBeCalledTimes(3);
     })
-  
-    test.values = new Set();
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(2);
-  })
-  
-  it("will subscribe to ANY if not accessed", () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.values);
+    
+    it("will update on clear", async () => {
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect($ => {
+        mock($.values.has("foo"));
+      })
+    
+      test.values.clear();
+      await test.update(true);
+    
+      expect(mock).toBeCalledTimes(2);
     })
-  
-  })
-  
-  describe("size", () => {
-    it("will update for any change", async () => {
+    
+    it("will update any key where iterated", async () => {
+      const test = Test.create();
+    
+      const mockIterator = jest.fn(({ values }: Test) => void [...values]);
+      const mockValues = jest.fn(({ values }: Test) => void values.values());
+      const mockKeys = jest.fn(({ values }: Test) => void values.keys());
+      const mockEntries = jest.fn(({ values }: Test) => void values.entries());
+    
+      test.effect(mockIterator);
+      test.effect(mockEntries);
+      test.effect(mockValues);
+      test.effect(mockKeys);
+    
+      test.insert("foo");
+      await test.update(true);
+    
+      expect(mockIterator).toBeCalledTimes(2);
+      expect(mockEntries).toBeCalledTimes(2);
+      expect(mockValues).toBeCalledTimes(2);
+      expect(mockKeys).toBeCalledTimes(2);
+    })
+    
+    it("will update any key where not accessed", () => {
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect($ => {
+        mock($.values);
+      })
+    })
+    
+    it("will update any key on replacement", async () => {
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect($ => {
+        mock($.values.has("foo"));
+      })
+    
+      test.values = new Set();
+      await test.update(true);
+    
+      expect(mock).toBeCalledTimes(2);
+    })
+    
+    it("will squash multiple updates", async () => {
+      const test = Test.create();
+      const mock = jest.fn((state: Test) => {
+        state.values.has("foo");
+        state.values.has("bar");
+      });
+    
+      test.effect(mock);
+    
+      test.insert("foo");
+      test.insert("bar");
+      await test.update(true);
+    
+      expect(mock).toBeCalledTimes(2);
+    })
+    
+    it("will spread multiple updates", async () => {
+      const test = Test.create();
+      const mock1 = jest.fn(($: Test) => void $.values.has(1));
+      const mock2 = jest.fn(($: Test) => void $.values.has(2));
+      const mock3 = jest.fn(($: Test) => void $.values.has(3));
+    
+      test.effect(mock1);
+      test.effect(mock2);
+      test.effect(mock3);
+    
+      test.insert(1);
+      test.insert(2);
+      test.insert(3);
+    
+      await test.update();
+    
+      expect(mock1).toBeCalledTimes(2);
+      expect(mock2).toBeCalledTimes(2);
+      expect(mock3).toBeCalledTimes(2);
+    })
+    
+    it("will allow normal methods outside proxy", () => {
+      const test = Test.create();
+    
+      test.insert("foo");
+      expect(test.values.has("foo")).toBe(true);
+    })
+    
+    it("will not update a stopped subscriber", async () => {
+      const test = Test.create();
+      const mock1 = jest.fn(($: Test) => void $.values);
+      const mock2 = jest.fn(($: Test) => void $.values);
+    
+      const release1 = test.effect(mock1);
+      const release2 = test.effect(mock2);
+    
+      expect(mock1).toBeCalledTimes(1);
+      expect(mock2).toBeCalledTimes(1);
+    
+      test.values = new Set();
+      await test.update(true);
+    
+      expect(mock1).toBeCalledTimes(2);
+      expect(mock2).toBeCalledTimes(2);
+    
+      release1();
+      test.values = new Set();
+      await test.update(true);
+    
+      expect(mock1).toBeCalledTimes(2);
+      expect(mock2).toBeCalledTimes(3);
+    
+      release2();
+    })
+
+    it("will update size for any change", async () => {
       const test = Test.create();
       const mock = jest.fn();
     
@@ -172,13 +234,13 @@ describe("set", () => {
     
       expect(mock).toBeCalledWith(0);
     
-      test.values.add("foo");
+      test.insert("foo");
       await test.update(true);
     
       expect(mock).toBeCalledWith(1);
     })
   
-    it("will update on replacement", async () => {
+    it("will update size on replacement", async () => {
       const test = Test.create();
       const mock = jest.fn();
     
@@ -193,289 +255,38 @@ describe("set", () => {
     
       expect(mock).toBeCalledWith(2);
     })
-  })
-  
-  it("will not memory-leak expired subscriber", async () => {
-    const test = Test.create();
-    const mock1 = jest.fn(($: Test) => void $.values);
-    const mock2 = jest.fn(($: Test) => void $.values);
-  
-    const release1 = test.effect(mock1);
-    const release2 = test.effect(mock2);
-  
-    expect(mock1).toBeCalledTimes(1);
-    expect(mock2).toBeCalledTimes(1);
-  
-    test.values = new Set();
-    await test.update(true);
-  
-    expect(mock1).toBeCalledTimes(2);
-    expect(mock2).toBeCalledTimes(2);
-  
-    release1();
-    test.values = new Set();
-    await test.update(true);
-  
-    expect(mock1).toBeCalledTimes(2);
-    expect(mock2).toBeCalledTimes(3);
-  
-    release2();
-  })
-})
-
-describe("map", () => {
-  class Test extends Model {
-    map = use(new Map());
-  }
-  
-  it("will update on set", async () => {
-    const test = Test.create();
-    const mock = jest.fn((state: Test) => {
-      state.map.get("foo");
-      state.map.has("bar");
-    });
-  
-    test.effect(mock);
-  
-    test.map.set("foo", "bar");
-    await test.update(true);
-  
-    test.map.set("bar", "foo");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(3);
-    test.destroy();
-  })
-  
-  it("will allow normal methods outside proxy", () => {
-    const { map } = Test.create();
-  
-    map.set("foo", "bar");
-    expect(map.get("foo")).toBe("bar");
-  })
-  
-  it("will squash simultaneous updates", async () => {
-    const test = Test.create();
-    const mock = jest.fn((state: Test) => {
-      state.map.get("foo");
-      state.map.has("bar");
-    });
-  
-    test.effect(mock);
-  
-    test.map.set("foo", "bar");
-    test.map.set("bar", "foo");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(2);
-  })
-  
-  it("will distrobute simultaneous updates", async () => {
-    const test = Test.create();
-    const mock1 = jest.fn(($: Test) => void $.map.has(1));
-    const mock2 = jest.fn(($: Test) => void $.map.has(2));
-    const mock3 = jest.fn(($: Test) => void $.map.has(3));
-  
-    test.effect(mock1);
-    test.effect(mock2);
-    test.effect(mock3);
-  
-    test.map.set(1, 1);
-    test.map.set(2, 2);
-    test.map.set(3, 3);
-  
-    await test.update();
-  
-    expect(mock1).toBeCalledTimes(2);
-    expect(mock2).toBeCalledTimes(2);
-    expect(mock3).toBeCalledTimes(2);
-  })
-  
-  it("will update on delete", async () => {
-    const test = Test.create();
-    const mock = jest.fn((state: Test) => {
-      state.map.get("foo");
-      state.map.has("bar");
-    });
-  
-    test.effect(mock);
-  
-    test.map.set("foo", "foo");
-    test.map.set("bar", "bar");
-    await test.update(true);
-  
-    test.map.delete("foo");
-    test.map.delete("bar");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(3);
-  })
-  
-  it("will not update on unwatched key", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.map.get("hello"));
+    
+    it("will update computed for used keys", async () => {
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect($ => mock($.size));
+    
+      expect(mock).toBeCalledWith(0);
+    
+      test.insert("foo");
+      await test.update(true);
+    
+      expect(mock).toBeCalledTimes(2)
+      expect(mock).toBeCalledWith(1);
     })
+    
+    it("will update computed for used keys only", async () => {
+      class Test extends Model {
+        values = use(new Set());
+        size = from(this, $ => $.values.has("bar"));
+      }
+    
+      const test = Test.create();
+      const mock = jest.fn();
+    
+      test.effect($ => mock($.size));
+    
+      expect(mock).toBeCalledWith(false);
+    
+      test.values.add("foo");
+      await test.update(true);
   
-    test.map.set("foo", "false");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(1);
-  })
-  
-  it("will update for any key where iterated", async () => {
-    const test = Test.create();
-  
-    const mockIterator = jest.fn(({ map }: Test) => void [...map]);
-    const mockValues = jest.fn(({ map }: Test) => void map.values());
-    const mockKeys = jest.fn(({ map }: Test) => void map.keys());
-    const mockEntries = jest.fn(({ map }: Test) => void map.entries());
-  
-    test.effect(mockIterator);
-    test.effect(mockEntries);
-    test.effect(mockValues);
-    test.effect(mockKeys);
-  
-    test.map.set("foo", "bar");
-    await test.update(true);
-  
-    expect(mockIterator).toBeCalledTimes(2);
-    expect(mockEntries).toBeCalledTimes(2);
-    expect(mockValues).toBeCalledTimes(2);
-    expect(mockKeys).toBeCalledTimes(2);
-  })
-  
-  it("will update on clear", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.map.get("hello"));
+      expect(mock).toBeCalledTimes(1);
     })
-  
-    test.map.clear();
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(2);
   })
-  
-  it("will update on new value", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.map.get("hello"));
-    })
-  
-    test.map = new Map();
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(2);
-  })
-  
-  it("will update size for any change", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.map.size);
-    })
-  
-    expect(mock).toBeCalledWith(0);
-  
-    test.map.set("foo", "bar");
-    await test.update(true);
-  
-    expect(mock).toBeCalledWith(1);
-  })
-  
-  it("will update size on full replacement", async () => {
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => {
-      mock($.map.size);
-    })
-  
-    expect(mock).toBeCalledWith(0);
-  
-    test.map = new Map([
-      ["foo", "foo"],
-      ["bar", "bar"]
-    ])
-  
-    await test.update(true);
-  
-    expect(mock).toBeCalledWith(2);
-  })
-  
-  it("will not memory-leak expired subscriber", async () => {
-    const test = Test.create();
-    const mock1 = jest.fn(($: Test) => void $.map);
-    const mock2 = jest.fn(($: Test) => void $.map);
-  
-    const release1 = test.effect(mock1);
-    const release2 = test.effect(mock2);
-  
-    expect(mock1).toBeCalledTimes(1);
-    expect(mock2).toBeCalledTimes(1);
-  
-    test.map = new Map();
-    await test.update(true);
-  
-    expect(mock1).toBeCalledTimes(2);
-    expect(mock2).toBeCalledTimes(2);
-  
-    release1();
-    test.map = new Map();
-    await test.update(true);
-  
-    expect(mock1).toBeCalledTimes(2);
-    expect(mock2).toBeCalledTimes(3);
-  
-    release2();
-  })
-})
-
-describe("computed", () => {
-  it("will trigger", async () => {
-    class Test extends Model {
-      values = use(new Set());
-      size = from(this, $ => $.values.size);
-    }
-  
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => mock($.size));
-  
-    expect(mock).toBeCalledWith(0);
-  
-    test.values.add("foo");
-    await test.update(true);
-  
-    expect(mock).toBeCalledTimes(2)
-    expect(mock).toBeCalledWith(1);
-  })
-  
-  it("will supress non-applicable", async () => {
-    class Test extends Model {
-      values = use(new Set());
-      size = from(this, $ => $.values.has("bar"));
-    }
-  
-    const test = Test.create();
-    const mock = jest.fn();
-  
-    test.effect($ => mock($.size));
-  
-    expect(mock).toBeCalledWith(false);
-  
-    test.values.add("foo");
-    await test.update(true);
-
-    expect(mock).toBeCalledTimes(1);
-  })
-})
