46d208f609367439c44333c59b1daabd9846d682
Gabe Klein <gabe@termtm.com>
2022-07-08 12:48:11 -0400

    dynamic control for keyed instruction

--- src/instruction/use/keyed.ts
+++ src/instruction/use/keyed.ts
@@ -1,6 +1,6 @@
 import { Controller } from '../../controller';
 import { Subscriber } from '../../subscriber';
-import { assign, create } from '../../util';
+import { assign, create, defineProperty } from '../../util';
 import { Instruction } from '../apply';
 
 const ANY = Symbol("any");
@@ -15,55 +15,135 @@ function keyed<T extends Set<any> | Map<any, any>>(
     T extends Map<infer U, any> ? U :
     never
   );
-
-  const managed = new Stateful(initial, emit);
-  const context = new WeakMap<Subscriber, typeof managed>();
-  const observers = new Set<(key: K) => void>();
+  const context = new Map<Subscriber, T>();
+  const users = new Map<Subscriber, Set<K>>();
+  const watched = new WeakMap<T, Set<K>>();
+  const update = new Set<(key: K) => void>();
   const frozen = new Set<Subscriber>();
+
+  let managed!: T;
+
+  manage(initial);
 
   function reset(){
     frozen.clear();
   }
 
+  function watch(on: any, key: K){
+    const include = watched.get(on);
+
+    if(include)
+      include.add(key);
+  }
+
   function emit(key: K){
     control.waiting.add(reset);
     control.update(property);
-    observers.forEach(notify => notify(key));
+    update.forEach(notify => notify(key));
   }
 
-  function init(local: Subscriber){
-    const proxy = create(managed);
-    const using = new Set<K>();
+  function manage(current: T){
+    managed = create(current);
+    context.clear();
 
-    function onEvent(key: K){
-      if(frozen.has(local))
-        return;
-
-      if(key === ANY || using.has(key) || using.has(ANY)){
-        const refresh = local.onUpdate(property, control);
-
-        if(refresh){
-          frozen.add(local);
-          refresh()
-        }
-      }
-    }
-
-    context.set(local, proxy);
-    proxy.watch = (key: K) => using.add(key);
-
-    local.add(property, false);
-    local.dependant.add({
-      commit(){
-        if(using.size === 0)
-          using.add(ANY);
-
-        observers.add(onEvent);
+    assign(managed,
+      <T>{
+        delete(key: K){
+          emit(key);
+          return current.delete(key);
+        },
+        clear(){
+          emit(ANY);
+          current.clear();
+        },
+        has(key: K){
+          watch(this, key);
+          return current.has(key);
+        },
+        keys(){
+          watch(this, ANY);
+          return current.keys();
+        },
+        values(){
+          watch(this, ANY);
+          return current.values();
+        },
+        entries(){
+          watch(this, ANY);
+          return current.entries();
+        },
+        [Symbol.iterator](){
+          watch(this, ANY);
+          return current[Symbol.iterator]();
+        },
       },
-      release(){
-        observers.delete(onEvent);
+      current instanceof Set
+        ? <T>{
+          add(key: K){
+            current.add(key);
+            emit(key);
+            return this;
+          }
+        }
+        : <T>{
+          get(key: K){
+            watch(this, ANY);
+            return current.get(key);
+          },
+          set(key: K, value: any){
+            current.set(key, value);
+            emit(key);
+            return this;
+          }
+        }
+    )
+
+    defineProperty(managed, "size", {
+      get(){
+        watch(this, ANY);
+        return current.size;
       }
     })
+  }
+
+  function subscribe(local: Subscriber){
+    const proxy = create(managed);
+    let watch = users.get(local);
+
+    if(!watch){
+      const using = watch = new Set<K>();
+      users.set(local, using);
+  
+      function onEvent(key: K){
+        if(frozen.has(local))
+          return;
+  
+        if(key === ANY || using.has(key) || using.has(ANY)){
+          const refresh = local.onUpdate(property, control);
+  
+          if(refresh){
+            frozen.add(local);
+            refresh()
+          }
+        }
+      }
+  
+      local.add(property, false);
+      local.dependant.add({
+        commit(){
+          if(using.size === 0)
+            using.add(ANY);
+  
+          update.add(onEvent);
+        },
+        release(){
+          update.delete(onEvent);
+        }
+      })
+    }
+
+    watched.set(proxy, watch);
+    context.set(local, proxy);
 
     return proxy;
   }
@@ -72,92 +152,18 @@ function keyed<T extends Set<any> | Map<any, any>>(
     value: initial,
     get(local){
       return local ?
-        context.get(local) || init(local) :
+        context.get(local) || subscribe(local) :
         managed;
     },
     set(next){
-      managed.source = next;
       control.state.set(property, next);
+      manage(next);
       emit(ANY);
     },
     destroy(){
-      observers.clear();
+      update.clear();
     }
   }
 }
 
 export { keyed };
-
-class Stateful<K, V = K> {
-  add?(key: K): this;
-  get?(key: K): V | undefined;
-  set?(key: K, value: V): this;
-
-  constructor(
-    public source: Set<K> | Map<K, V>,
-    private emit: (key: K | typeof ANY) => void
-  ){
-    if(source instanceof Set)
-      assign(this, <any>{
-        add(key: K){
-          source.add(key);
-          emit(key);
-          return this;
-        }
-      })
-    else
-      assign(this, <any>{
-        get(key: K){
-          this.watch(key);
-          return source.get(key);
-        },
-        set(key: K, value: V){
-          source.set(key, value);
-          emit(key);
-          return this;
-        }
-      })
-  }
-
-  watch(_key: K | typeof ANY){}
-
-  has(key: K){
-    this.watch(key);
-    return this.source.has(key);
-  };
-
-  delete(key: K){
-    this.emit(key);
-    return this.source.delete(key);
-  };
-
-  clear(){
-    this.emit(ANY);
-    this.source.clear();
-  }
-
-  keys(){
-    this.watch(ANY);
-    return this.source.keys();
-  }
-
-  values(){
-    this.watch(ANY);
-    return this.source.values();
-  }
-
-  entries(){
-    this.watch(ANY);
-    return this.source.entries();
-  };
-
-  get size(){
-    this.watch(ANY);
-    return this.source.size;
-  }
-
-  [Symbol.iterator](){
-    this.watch(ANY);
-    return this.source[Symbol.iterator]();
-  };
-}
