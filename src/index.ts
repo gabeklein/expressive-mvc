da0c504dc523d89faebf8268b985e4fa991644a2
Gabe Klein <gabe@termtm.com>
2022-07-09 19:37:39 -0400

    delete parent, from instructions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { act } from './instruction/act';
 export { apply } from './instruction/apply';
-export { get, get as from } from './instruction/get';
+export { get } from './instruction/get';
 export { ref } from './instruction/ref';
 export { put } from './instruction/put';
 export { set } from './instruction/set';
 export { use } from './instruction/use/use';
-export { has, has as parent } from './instruction/has';
+export { has } from './instruction/has';
 
 export { MVC as Model, MVC as default } from './react/mvc';
 export { CONTROL, LOCAL, STATE, WHY, Stateful } from './model';
