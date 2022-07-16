9fa36129052fd517d149ce325529e74b3dc920c6
Gabe Klein <gabe@termtm.com>
2022-07-16 11:27:02 -0400

    remove subscribeTo helper from tests

--- src/instruction/use.test.ts
+++ src/instruction/use.test.ts
@@ -13,7 +13,7 @@ class Parent extends Model {
 
 it('will track recursively', async () => {
   const state = Parent.create();
-  const update = subscribeTo(state, it => {
+  const mock = jest.fn((it: Parent) => {
     void it.value;
     void it.child.value;
   })
@@ -21,11 +21,15 @@ it('will track recursively', async () => {
   expect(state.value).toBe("foo");
   expect(state.child.value).toBe("foo");
 
+  state.effect(mock);
+
   state.value = "bar";
-  await update();
+  await state.update(true);
+  expect(mock).toHaveBeenCalledTimes(2)
 
   state.child.value = "bar";
-  await update();
+  await state.child.update(true);
+  expect(mock).toHaveBeenCalledTimes(3)
 })
 
 it('will accept instance', async () => {
@@ -36,14 +40,18 @@ it('will accept instance', async () => {
   }
 
   const state = Parent.create();
-  const update = subscribeTo(state, it => {
+  const mock = jest.fn((it: Parent) => {
     void it.child.value;
   })
+
+  state.effect(mock);
 
   expect(state.child.value).toBe("foo");
 
   state.child.value = "bar";
-  await update();
+  await state.child.update(true);
+
+  expect(mock).toBeCalledTimes(2);
 
   expect(state.child.value).toBe("bar");
 })
@@ -66,6 +74,13 @@ it('will accept simple object', async () => {
   }
 
   const state = Parent.create();
+  const mock = jest.fn((it: Parent) => {
+    void it.child.value;
+  })
+
+  state.effect(mock);
+
+  // TODO: remove subscribeTo helper
   const update = subscribeTo(state, it => {
     void it.child.value;
   })
@@ -84,6 +99,7 @@ it('will accept simple object as new value', async () => {
   }
 
   const state = Parent.create();
+  // TODO: remove subscribeTo helper
   const update = subscribeTo(state, it => {
     void it.child.value;
   })
@@ -136,27 +152,32 @@ it('will accept undefined from factory', async () => {
 
 it('will update on new value', async () => {
   const state = Parent.create();
-  const update = subscribeTo(state, it => {
+  const mock = jest.fn((it: Parent) => {
     void it.value;
     void it.child.value;
   })
+
+  state.effect(mock);
 
   expect(state.child.value).toBe("foo");
 
   // Will refresh on sub-value change.
   state.child.value = "bar";
-  await update();
+  await state.child.update(true);
   expect(state.child.value).toBe("bar");
+  expect(mock).toBeCalledTimes(2);
 
   // Will refresh on repalcement.
   state.child = new Child();
-  await update();
+  await state.update(true);
   expect(state.child.value).toBe("foo");
+  expect(mock).toBeCalledTimes(3);
 
   // New subscription still works.
   state.child.value = "bar";
-  await update();
+  await state.child.update(true);
   expect(state.child.value).toBe("bar");
+  expect(mock).toBeCalledTimes(4);
 })
 
 it('will reset if value is undefined', async () => {
@@ -166,33 +187,39 @@ it('will reset if value is undefined', async () => {
   }
 
   const state = Parent.create();
-  const update = subscribeTo(state, it => {
+  const mock = jest.fn((it: Parent) => {
     void it.value;
 
     if(it.child)
       void it.child.value;
   })
 
-  state.child = new Child();
+  state.effect(mock);
 
-  await update();
+  state.child = new Child();
+  await state.update(true);
+  expect(mock).toBeCalledTimes(2)
 
   // Will refresh on sub-value change.
-  state.child!.value = "bar";
-  await update();
+  state.child.value = "bar";
+  await state.child.update(true);
+  expect(mock).toBeCalledTimes(3);
 
   // Will refresh on undefined.
   state.child = undefined;
-  await update();
+  await state.update(true);
   expect(state.child).toBeUndefined();
+  expect(mock).toBeCalledTimes(4);
 
   // Will refresh on repalcement.
   state.child = new Child();
-  await update();
+  await state.update(true);
+  expect(mock).toBeCalledTimes(5);
 
   // New subscription still works.
   state.child.value = "bar";
-  await update();
+  await state.child.update(true);
+  expect(mock).toBeCalledTimes(6);
 })
 
 it('will still subscribe if initially undefined', async () => {
@@ -202,26 +229,30 @@ it('will still subscribe if initially undefined', async () => {
   }
 
   const state = Parent.create();
-  const update = subscribeTo(state, it => {
+  const mock = jest.fn((it: Parent) => {
     void it.value;
 
     if(it.child)
       void it.child.value;
   })
 
+  state.effect(mock);
   expect(state.child).toBeUndefined();
 
   // Will refresh on repalcement.
   state.child = new Child();
-  await update();
+  await state.update(true);
+  expect(mock).toBeCalledTimes(2)
 
   // New subscription does work.
   state.child.value = "bar";
-  await update();
+  await state.child.update(true);
+  expect(mock).toBeCalledTimes(3)
 
   // Will refresh on deletion.
   state.child = undefined;
-  await update();
+  await state.update(true);
+  expect(mock).toBeCalledTimes(4)
 })
 
 it('will throw if bad argument type', () => {
