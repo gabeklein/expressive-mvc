d3fa1b632760d89c48d6577f5eef0f781af58883
Gabe Klein <gabe@termtm.com>
2023-01-02 10:46:46 -0500

    improved signature for useTap function

--- src/react/useTap.ts
+++ src/react/useTap.ts
@@ -1,48 +1,55 @@
 import React, { useMemo } from 'react';
 
 import { ensure } from '../controller';
-import { Model, Stateful } from '../model';
+import { Model } from '../model';
 import { suspend } from '../suspense';
 import { use } from './use';
 import { useFrom } from './useFrom';
 import { useLocal } from './useLocal';
 
-namespace useTap {
-  export interface Tappable {
-    new (): Stateful;
-    new (...args: any[]): Stateful;
-    get?: () => Stateful;
+declare namespace useTap {
+  type SourceType<T extends Model> = {
+    new(): T;
+    get?(): T
   }
+
+  type Source<T extends Model> =
+    | T
+    | SourceType<T>
+    | Model.Type<T>
+    | (() => T | Model.Type<T>);
 }
 
-function useTap <T extends Stateful> (source: T | (() => T)): T;
+function useTap <T extends Model> (
+  source: useTap.Source<T>
+  ): T;
 
-function useTap <T extends Stateful, K extends Model.Field<T>> (
-  source: T | (() => T) | useTap.Tappable,
+function useTap <T extends Model, K extends Model.Field<T>> (
+  source: useTap.Source<T>,
   path: K,
   expect: true
 ): Exclude<T[K], undefined>;
 
-function useTap <T extends Stateful, K extends Model.Field<T>> (
-  source: T | (() => T) | useTap.Tappable,
+function useTap <T extends Model, K extends Model.Field<T>> (
+  source: useTap.Source<T>,
   path: K,
   expect?: boolean
 ): T[K];
 
-function useTap <T extends Stateful, R> (
-  source: T | (() => T) | useTap.Tappable,
+function useTap <T extends Model, R> (
+  source: useTap.Source<T>,
   compute: (this: T, from: T) => R,
   expect: true
 ): Exclude<R, undefined>;
 
-function useTap <T extends Stateful, R> (
-  source: T | (() => T) | useTap.Tappable,
+function useTap <T extends Model, R> (
+  source: useTap.Source<T>,
   compute: (this: T, from: T) => R,
   expect?: boolean
 ): R;
 
-function useTap <T extends Stateful> (
-  source: T | (() => T) | useTap.Tappable,
+function useTap <T extends Model> (
+  source: useTap.Source<T>,
   path?: Model.Field<T> | ((this: T, from: T) => any),
   expect?: boolean) {
 
