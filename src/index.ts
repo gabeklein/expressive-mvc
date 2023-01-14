494b68038b92fb8c11ac4538f37bc3acca95dbdf
Gabe Klein <gabe@termtm.com>
2023-01-14 01:29:06 -0500

    reorder exports

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ export { use } from './instruction/use';
 export { has } from './instruction/has';
 
 export { MVC, MVC as Model, MVC as default } from './react/mvc';
-export { Debug } from './debug';
+export { useValue } from './react/useValue';
+export { useContext } from './react/context';
+export { useModel } from './react/useModel';
+export { useTap } from './react/useTap';
 export { Consumer } from './react/consumer';
 export { Provider } from './react/provider';
 export { Global } from './react/global';
 export { tap } from './react/tap';
 
-export { useValue } from './react/useValue';
-export { useContext } from './react/context';
-export { useModel } from './react/useModel';
-export { useTap } from './react/useTap';
+export { Debug } from './debug';
