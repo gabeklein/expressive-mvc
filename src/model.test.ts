1b6f2b47dcc60ba1e30d7efaf9856b4174e1746a
Gabe Klein <gabe@termtm.com>
2023-01-13 22:50:18 -0500

    wrap Model tests in describe

--- src/model.test.ts
+++ src/model.test.ts
@@ -1,91 +1,93 @@
 import { Model } from './model';
 
-class Subject extends Model {
-  value: number;
-
-  constructor(initial?: number){
-    super();
-    this.value = initial || 1;
-  }
-
-  setValue = (to: number) => {
-    this.value = to;
-  }
-}
-
-it('will instantiate from custom class', () => {
-  const state = Subject.new();
-
-  expect(state.value).toBe(1);
-})
-
-it('will send arguments to constructor', () => {
-  const state = Subject.new(3);
-
-  expect(state.value).toBe(3);
-})
-
-it('will assign is as a circular reference', async () => {
-  const state = Subject.new();
-
-  expect(state.is.value).toBe(1);
-
-  state.value = 2;
-  await state.on(true);
-
-  expect(state.is.value).toBe(2)
-})
-
-it("will ignore getters and setters", () => {
-  class Test extends Model {
-    foo = "foo";
-
-    get bar(){
-      return "bar";
-    }
-  }
-
-  const test = Test.new();
-
-  expect(test.bar).toBe("bar");
-  expect(test.export()).not.toContain("bar");
-})
-
-it('will update when a value changes', async () => {
-  const state = Subject.new();
-
-  expect(state.value).toBe(1);
-
-  state.value = 2
-  await state.on(true);
-
-  expect(state.value).toBe(2);
-})
-
-it('will not update if value is same', async () => {
-  const state = Subject.new();
-
-  expect(state.value).toBe(1);
-
-  state.value = 1
-  await state.on(null);
-})
-
-it('accepts update from within a method', async () => {
+describe("Model", () => {
   class Subject extends Model {
-    value = 1;
-
+    value: number;
+  
+    constructor(initial?: number){
+      super();
+      this.value = initial || 1;
+    }
+  
     setValue = (to: number) => {
       this.value = to;
     }
   }
-
-  const state = Subject.new();
-
-  state.setValue(3);
-  await state.on(true);
-
-  expect(state.value).toBe(3)
+  
+  it('will instantiate from custom class', () => {
+    const state = Subject.new();
+  
+    expect(state.value).toBe(1);
+  })
+  
+  it('will send arguments to constructor', () => {
+    const state = Subject.new(3);
+  
+    expect(state.value).toBe(3);
+  })
+  
+  it('will assign is as a circular reference', async () => {
+    const state = Subject.new();
+  
+    expect(state.is.value).toBe(1);
+  
+    state.value = 2;
+    await state.on(true);
+  
+    expect(state.is.value).toBe(2)
+  })
+  
+  it("will ignore getters and setters", () => {
+    class Test extends Model {
+      foo = "foo";
+  
+      get bar(){
+        return "bar";
+      }
+    }
+  
+    const test = Test.new();
+  
+    expect(test.bar).toBe("bar");
+    expect(test.export()).not.toContain("bar");
+  })
+  
+  it('will update when a value changes', async () => {
+    const state = Subject.new();
+  
+    expect(state.value).toBe(1);
+  
+    state.value = 2
+    await state.on(true);
+  
+    expect(state.value).toBe(2);
+  })
+  
+  it('will not update if value is same', async () => {
+    const state = Subject.new();
+  
+    expect(state.value).toBe(1);
+  
+    state.value = 1
+    await state.on(null);
+  })
+  
+  it('accepts update from within a method', async () => {
+    class Subject extends Model {
+      value = 1;
+  
+      setValue = (to: number) => {
+        this.value = to;
+      }
+    }
+  
+    const state = Subject.new();
+  
+    state.setValue(3);
+    await state.on(true);
+  
+    expect(state.value).toBe(3)
+  })
 })
 
 describe("dispatch", () => {
