367ec1c6fdb67b169d6f28ba84d260358941383e
Gabe Klein <gabe@termtm.com>
2023-01-20 16:53:14 -0500

    add test for accumulation

--- src/react/mvc.test.tsx
+++ src/react/mvc.test.tsx
@@ -199,4 +199,51 @@ describe("tap", () => {
       </Provider>
     );
   })
+
+  it("will run initial callback syncronously", async () => {
+    class Parent extends MVC {
+      values = [] as string[]
+    }
+    
+    type ChildProps = {
+      value: string;
+    }
+
+    const Child = (props: ChildProps) => {
+      Parent.tap($ => {
+        didPushToValues();
+        $.values.push(props.value);
+        $.set("values");
+
+        return () => null;
+      });
+
+      return null;
+    }
+
+    const parent = Parent.new();
+    const didUpdateValues = jest.fn();
+    const didPushToValues = jest.fn();
+
+    parent.on("values", didUpdateValues);
+
+    const element = render(
+      <Provider for={parent}>
+        <Child value='foo' />
+        <Child value='bar' />
+        <Child value='baz' />
+      </Provider>
+    )
+
+    expect(didPushToValues).toBeCalledTimes(3);
+
+    await parent.on(true);
+
+    expect(parent.values.length).toBe(3);
+
+    // Expect updates to have bunched up before new frame.
+    expect(didUpdateValues).toBeCalledTimes(1);
+
+    element.unmount();
+  })
 })
