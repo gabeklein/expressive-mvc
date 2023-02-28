f7b1fdaedf76ddfc91228e425bba7562ca4f8fda
Gabe Klein <gabe@termtm.com>
2023-02-28 09:01:41 -0500

    collapse get.factory into set

--- src/instruction/set.ts
+++ src/instruction/set.ts
@@ -1,12 +1,16 @@
+import { Control } from '../control';
 import { createValueEffect } from '../effect';
 import { issues } from '../helper/issues';
+import { assign } from '../helper/object';
 import { mayRetry, suspend } from '../suspense';
 import { add } from './add';
-import { factoryMode } from './get.factory';
 
 export const Oops = issues({
   ComputeFailed: (model, key) =>
     `Generating initial value for ${model}.${key} failed.`,
+
+  NotReady: (model, key) =>
+    `Value ${model}.${key} value is not yet available.`
 });
 
 declare namespace set {
@@ -124,6 +128,60 @@ function set <T> (
       }
     }
   )
+}
+
+export function factoryMode<T>(
+  self: Control,
+  value: Promise<T> | T,
+  key: string,
+  required: boolean
+){
+  const { subject, state } = self;
+
+  let pending: Promise<any> | undefined;
+  let error: any;
+
+  if(value instanceof Promise){
+    state.set(key, undefined);
+
+    pending = value
+      .catch(err => error = err)
+      .then(val => {
+        state.set(key, val);
+        return val;
+      })
+      .finally(() => {
+        pending = undefined;
+        self.update(key);
+      })
+  }
+  else
+    state.set(key, value);
+
+  const suspend = () => {
+    if(required === false)
+      return undefined;
+
+    const issue = Oops.NotReady(subject, key);
+
+    assign(pending!, {
+      message: issue.message,
+      stack: issue.stack
+    });
+
+    throw pending;
+  }
+  
+  return (): T | undefined => {
+    if(error)
+      throw error;
+
+    if(pending)
+      return suspend();
+
+    if(state.has(key))
+      return state.get(key);
+  }
 }
 
 export { set }
