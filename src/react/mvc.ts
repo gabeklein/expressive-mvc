3229d826f07999058ab50dac7494a0cb02af5663
Gabe Klein <gabe@termtm.com>
2023-03-13 09:20:58 -0400

    remove method comments from MVC

--- src/react/mvc.ts
+++ src/react/mvc.ts
@@ -33,26 +33,8 @@ declare namespace MVC {
 }
 
 class MVC extends Model {
-  /**
-   * **React Hook** - Fetch most instance of this controller from context, if it exists.
-   * 
-   * @param required - Unless false, will throw where instance cannot be found.
-   */
   static get <T extends MVC> (this: Model.Type<T>, required?: boolean, effectCallback?: MVC.EffectCallback<T>): T;
-
-  /**
-   * **React Hook** - Fetch most instance of this controller from context.
-   * 
-   * @param required - If false, may return undefined.
-   */
   static get <T extends MVC> (this: Model.Type<T>, required: false, effectCallback?: MVC.EffectCallback<T | undefined>): T | undefined;
-
-  /**
-   * **React Hook** - Fetch instance.
-   * 
-   * Effect callback will run once if found, throw if not found.
-   * Returned function is called on unmount.
-   */
   static get <I extends MVC> (this: Model.Type<I>, effectCallback: MVC.EffectCallback<I>): I | undefined;
 
   static get(
@@ -69,9 +51,6 @@ class MVC extends Model {
     return instance;
   }
 
-  /** 
-   * **React Hook** - Fetch and subscribe to instance of this controller within ambient component.
-   */
   static tap <T extends MVC> (this: Model.Type<T>): T;
   static tap <T extends MVC, R extends readonly unknown[] | []> (this: Model.Type<T>, compute: MVC.TapCallback<T, R | (() => R)>, expect?: boolean): R;
   static tap <T extends MVC, R extends readonly unknown[] | []> (this: Model.Type<T>, compute: MVC.TapCallback<T, Promise<R> | (() => R) | null>, expect?: boolean): R | null;
