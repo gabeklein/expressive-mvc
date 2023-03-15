4fecce9c8efe6b8f2bce7ddd3719126416d05cf9
Gabe Klein <gabe@termtm.com>
2023-03-15 14:55:17 -0400

    remove react from core mvc

--- packages/mvc/src/index.ts
+++ packages/mvc/src/index.ts
@@ -1,3 +1,6 @@
+import { getParent } from './children';
+import { setFindFunction } from './instruction/get';
+
 export { add } from './instruction/add';
 export { run } from './instruction/run';
 export { get } from './instruction/get';
@@ -5,10 +8,13 @@ export { ref } from './instruction/ref';
 export { set } from './instruction/set';
 export { use } from './instruction/use';
 
-export { Model } from './react/mvc';
-export { Consumer } from './react/consumer';
-export { Provider } from './react/provider';
-
+export { Model } from './model';
 export { Control } from './control';
 export { Subscriber } from './subscriber';
 export { Debug } from './debug';
+export { Register } from './register';
+
+export const Internal = {
+  setFindFunction,
+  getParent
+}
