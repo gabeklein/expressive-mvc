1a1749717c0a68033d9e418dc67a17db1bedd68e
Gabe Klein <gabe@termtm.com>
2023-03-20 23:54:42 -0400

    don't bother with coverage in root tests

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   testTimeout: 1000,
-  coveragePathIgnorePatterns: [
-    "/src/index.ts"
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
   projects: [
     {
       preset: "ts-jest",
