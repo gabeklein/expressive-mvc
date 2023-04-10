3f4b6549e48a0738dff3cbee1176e9f48c8675af
Gabe Klein <gabe@termtm.com>
2023-04-10 09:00:46 -0400

    syntax

--- packages/mvc/src/instruction/add.ts
+++ packages/mvc/src/instruction/add.ts
@@ -1,7 +1,5 @@
 import { Control, PENDING } from '../control';
 
-
-
 /**
  * Run instruction as controller sets itself up.
  * This will specialize the behavior of a given property.
@@ -10,8 +8,9 @@ function add<T = any>(
   instruction: Control.Instruction<any>,
   label?: string){
 
-  const name = label || instruction.name || "pending";
-  const placeholder = Symbol(name + " instruction");
+  const placeholder = Symbol((
+    label || instruction.name || "pending"
+  ) + " instruction");
 
   PENDING.set(placeholder, (control: Control, key: string) => {
     let output = instruction.call(control, key, control);
