de2b6f84d8c9cbd5a55082e4c38e80660c0083c9
Gabe Klein <gabe@termtm.com>
2023-04-10 14:40:43 -0400

    move add function to top

--- packages/mvc/src/instruction/add.ts
+++ packages/mvc/src/instruction/add.ts
@@ -2,6 +2,22 @@ import { Control } from '../control';
 
 const PENDING = new Map<symbol, Control.Instruction<any>>();
 
+/**
+ * Run instruction as controller sets itself up.
+ * This will specialize the behavior of a given property.
+ */
+export function add<T = any>(
+  instruction: Control.Instruction<any>,
+  label?: string){
+
+  const name = label || instruction.name || "pending";
+  const placeholder = Symbol(name + " instruction");
+
+  PENDING.set(placeholder, instruction);
+
+  return placeholder as unknown as T;
+}
+
 export function setInstruction(
   from: symbol,
   onto: Control,
@@ -21,20 +37,4 @@ export function setInstruction(
     return { get: output };
 
   return output;
-}
-
-/**
- * Run instruction as controller sets itself up.
- * This will specialize the behavior of a given property.
- */
-export function add<T = any>(
-  instruction: Control.Instruction<any>,
-  label?: string){
-
-  const name = label || instruction.name || "pending";
-  const placeholder = Symbol(name + " instruction");
-
-  PENDING.set(placeholder, instruction);
-
-  return placeholder as unknown as T;
 }
