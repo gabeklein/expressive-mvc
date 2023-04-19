059efa7a8054598bba298f6234b77348b0d1af21
Gabe Klein <gabe@termtm.com>
2023-04-19 03:18:34 -0400

    invert messages for toUpdate assertion

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,10 +6,10 @@ expect.extend({
 
     return didUpdate ? {
       pass: true,
-      message: () => `Expected ${received} to have pending update.`
+      message: () => `Expected ${received} not to have pending update.`
     } : {
       pass: false,
-      message: () => `Expected ${received} not to have a pending update.`
+      message: () => `Expected ${received} to have a pending update.`
     };
   }
 })
