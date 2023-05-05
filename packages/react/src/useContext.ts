6bf07c8366376b093fae35f4c8dffba29a05e76d
Gabe Klein <gabe@termtm.com>
2023-05-05 13:17:31 -0400

    syntax

--- packages/react/src/useContext.ts
+++ packages/react/src/useContext.ts
@@ -4,7 +4,8 @@ export function useContext<T>(
   factory: (update: () => void) => void | {
     commit: () => (() => void) | void;
     render: () => T;
-  }){
+  }
+){
   const state = useState(0);
   const hook = useMemo(() => {
     const result = factory(() => state[1](x => x+1));
