c10186fc6626294a55a41ff93a57c3077952c3bd
Gabe Klein <gabe@termtm.com>
2023-05-06 14:31:03 -0400

    remove consolidated get-method tests from react

--- packages/react/src/helper/testing.tsx
+++ packages/react/src/helper/testing.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 import { Model } from '@expressive/mvc';
 import { renderHook, WrapperComponent } from '@testing-library/react-hooks';
-import { Suspense } from 'react';
-import { create } from 'react-test-renderer';
+import React from 'react';
+
 import { Provider } from '../provider';
 
 export { renderHook } from '@testing-library/react-hooks';
@@ -30,68 +28,6 @@ export function mockAsync<T = void>(){
   return {
     pending: event,
     resolve
-  }
-}
-
-export function mockSuspense(provide?: Model){
-  const promise = mockAsync();
-
-  let renderHook!: () => void;
-  let didRender = false;
-  let didSuspend = false;
-
-  const reset = () => {
-    didSuspend = didRender = false;
-  }
-
-  const Waiting = () => {
-    didSuspend = true;
-    return null;
-  }
-
-  const Component = () => {
-    try {
-      didRender = true;
-      renderHook();
-    }
-    finally {
-      promise.resolve();
-    }
-
-    return null;
-  }
-
-  return {
-    waitForNextRender(){
-      return promise.pending();
-    },
-    renderHook(fn: () => void){
-      renderHook = fn;
-
-      let render = (
-        <Suspense fallback={<Waiting />}>
-          <Component />
-        </Suspense>
-      )
-
-      if(provide)
-        render = (
-          <Provider for={provide}>
-            {render}
-          </Provider>
-        )
-
-      create(render);
-    },
-    assertDidRender(yes: boolean){
-      expect(didRender).toBe(yes);
-      expect(didSuspend).toBe(false);
-      reset();
-    },
-    assertDidSuspend(yes: boolean){
-      expect(didSuspend).toBe(yes);
-      reset();
-    }
   }
 }
 
