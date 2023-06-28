1614ff30da81a0c67468abaf1724609ee75505bd
Gabe Klein <gabe@termtm.com>
2023-06-28 16:45:27 -0400

    webpack plugin?

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,32 @@
 const nextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = nextra()
+const withNextra = nextra();
+
+module.exports = {
+  ...withNextra,
+  webpack: (config, options) => {
+    config = withNextra.webpack(config, options);
+
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-typescript",
+              "@expressive/babel-preset-react"
+            ]
+          }
+        },
+      ],
+    })
+
+    return config;
+  }
+}
---
+++ docs/shared/hello.js
@@ -0,0 +1,5 @@
+export const Hello = () => {
+  color: red;
+
+  <h1>Hello!!!!</h1>
+}
