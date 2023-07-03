de2d42690a93ca5cc1f7498fa4444ca4663f8c7a
Gabe Klein <gabe@termtm.com>
2023-07-02 20:39:53 -0400

    install expressive webpack plugin

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,36 +1,15 @@
-import Nextra from 'nextra'
+import Nextra from 'nextra';
+import ExpressivePlugin from '@expressive/webpack-plugin';
 
 const nextra = Nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 });
 
-const withNextra = nextra();
-
-export default {
-  ...withNextra,
-  webpack: (config, options) => {
-    config = withNextra.webpack(config, options);
-
-    config.module.rules.push({
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "@babel/preset-typescript",
-              ["@expressive/babel-preset-react", {
-
-              }]
-            ]
-          }
-        },
-      ],
-    })
-
-    return config;
+export default nextra({
+  webpack: (config) => {
+    config.plugins.push(
+      new ExpressivePlugin()
+    );
   }
-}
+});
