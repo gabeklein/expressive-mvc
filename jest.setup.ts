d6ba850bbfec6b51fb43fd7bb99c47bb3fff49cd
Gabe Klein <gabe@termtm.com>
2023-09-07 00:08:14 -0400

    better syntax

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,40 +1,41 @@
 import { Model } from "@expressive/mvc";
 
-expect.extend({
-  async toUpdate(received: Model){
-    const didUpdate = await received.set();
+async function toUpdate(received: Model){
+  const didUpdate = await received.set();
 
-    return didUpdate ? {
-      pass: true,
-      message: () => `Expected ${received} not to have pending updates.`
-    } : {
+  return didUpdate ? {
+    pass: true,
+    message: () => `Expected ${received} not to have pending updates.`
+  } : {
+    pass: false,
+    message: () => `Expected ${received} to have pending updates.`
+  };
+}
+
+async function toHaveUpdated(received: Model, ...keys: string[]){
+  const didUpdate = await received.set();
+
+  if(!didUpdate)
+    return {
       pass: false,
       message: () => `Expected ${received} to have pending updates.`
-    };
-  },
+    }
 
-  async toHaveUpdated(received: Model, ...keys: string[]){
-    const didUpdate = await received.set();
+  const got = Object.keys(didUpdate);
+  const equal = JSON.stringify(got.sort()) === JSON.stringify(keys.sort());
 
-    if(!didUpdate)
-      return {
-        pass: false,
-        message: () => `Expected ${received} to have pending updates.`
-      }
+  return equal ? {
+    pass: true,
+    message: () =>
+      `Expected ${received} not to have updated keys [${keys.join(", ")}].`
+  } : {
+    pass: false,
+    message: () =>
+      `Expected ${received} to have updated keys [${keys.join(", ")}] but got [${got.join(", ")}].`
+  };
+}
 
-    const got = Object.keys(didUpdate);
-    const equal = JSON.stringify(got.sort()) === JSON.stringify(keys.sort());
-
-    return equal ? {
-      pass: true,
-      message: () => (
-        `Expected ${received} not to have updated keys [${keys.join(", ")}].`
-      )
-    } : {
-      pass: false,
-      message: () => (
-        `Expected ${received} to have updated keys [${keys.join(", ")}] but got [${got.join(", ")}].`
-      )
-    };
-  }
+expect.extend({
+  toUpdate,
+  toHaveUpdated
 })
