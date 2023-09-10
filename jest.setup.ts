c5fbc4fa3d204e88d3700e2deeeffd2e7edaf76e
Gabe Klein <gabe@termtm.com>
2023-09-10 15:38:40 -0400

    syntax

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,32 +1,32 @@
 import { Model } from "@expressive/mvc";
 
-async function toUpdate(received: Model, ...keys: string[]){
-  const didUpdate = await received.set();
-
-  if(!didUpdate)
-    return {
-      pass: false,
-      message: () => `Expected ${received} to have pending updates.`
-    }
-
-  if(!keys.length)
-    return {
+expect.extend({
+  async toUpdate(received: Model, ...keys: string[]){
+    const didUpdate = await received.set();
+  
+    if(!didUpdate)
+      return {
+        pass: false,
+        message: () => `Expected ${received} to have pending updates.`
+      }
+  
+    if(!keys.length)
+      return {
+        pass: true,
+        message: () => `Expected ${received} not to have pending updates.`
+      }
+  
+    const got = Object.keys(didUpdate);
+    const equal = JSON.stringify(got.sort()) === JSON.stringify(keys.sort());
+  
+    return equal ? {
       pass: true,
-      message: () => `Expected ${received} not to have pending updates.`
-    }
-
-  const got = Object.keys(didUpdate);
-  const equal = JSON.stringify(got.sort()) === JSON.stringify(keys.sort());
-
-  return equal ? {
-    pass: true,
-    message: () =>
-      `Expected ${received} not to have updated keys [${keys.join(", ")}].`
-  } : {
-    pass: false,
-    message: () =>
-      `Expected ${received} to have updated keys [${keys.join(", ")}] but got [${got.join(", ")}].`
-  };
-}
-
-expect.extend({ toUpdate })
+      message: () =>
+        `Expected ${received} not to have updated keys [${keys.join(", ")}].`
+    } : {
+      pass: false,
+      message: () =>
+        `Expected ${received} to have updated keys [${keys.join(", ")}] but got [${got.join(", ")}].`
+    };
+  }
+})
