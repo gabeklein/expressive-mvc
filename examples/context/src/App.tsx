8bf770b488d835a288243646052ad0b17d31496c
Gabe Klein <gabe@termtm.com>
2023-09-12 20:25:22 -0400

    implement context example

---
+++ examples/context/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Demo from './Demo';
+
+createRoot(
+  document.getElementById('root')!
+).render(<Demo />)
