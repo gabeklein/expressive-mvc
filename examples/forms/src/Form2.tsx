b809dec7c537e04d9f4095353e37a95328dd581a
Gabe Klein <gabe@termtm.com>
2023-09-17 14:09:21 -0400

    untracked files on develop: 99731f0e improve example

---
+++ examples/forms/src/Form2.tsx
@@ -0,0 +1,82 @@
+import { Model } from '@expressive/react';
+import React, { InputHTMLAttributes, Ref } from 'react';
+
+declare class FancyMap<K, T> extends Map<K, T> {
+  get(key: K): T | undefined;
+  get(key: K, callback: (value: T) => void): () => void;
+}
+
+declare function map<T>(): FancyMap<string, T>
+declare function map<K = string, T = unknown>(): FancyMap<K, T>
+
+/*
+  Form here will be a Model which may be used to control
+  *any* form in a hypothetical application. It will be used to
+  demonstrate how to extend the Model.get static method to make a
+  custom Model usable in different ways.
+
+  How this works can be completely up to you.
+  Mainly we want to show there's no need to delegate
+  for simple features as you build an app.
+*/
+class Form extends Model {
+  values = map<string>();
+
+  /*
+    For reusability, create a static method to wrap process
+    for binding an <input> (or textarea) to a property on a model.
+
+    Here `this.get()` [read: Form.get static method]
+    will create a ref-function that will be used by React.
+
+    Not only will `.get()` fetch the nearest instance of Form,
+    it will pass that instance to a function, which will then
+    return a result and be memoized by component going forward.
+  */
+  static createRef(property: string): Ref<HTMLInputElement> {
+    return this.get(self => {
+      let reset: (() => void) | undefined;
+      const { values } = self;
+
+      return (input) => {
+        if(reset)
+          reset();
+
+        if(!input)
+          return;
+
+        const unfollow = values.get(property, x => input.value = x);
+        const onInput = () => values.set(property, input.value);
+      
+        if(values.has(property))
+          input.value = values.get(property)!;
+        else
+          onInput();
+
+        input.addEventListener("input", onInput);
+
+        reset = () => {
+          input.removeEventListener("input", onInput);
+          unfollow();
+        };
+      }
+    })
+  }
+}
+
+/*
+  Next, we create a reusable Input component to used
+  in conjunction with Form. This allows us to create a
+  styled component which can communicate with any Model
+  to extend (or contains) the Form class. This way, there's
+  no need to pass any props for controlling the input.
+*/
+const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  const ref = Form.createRef(props.name!);
+
+  return (
+    <input {...props} ref={ref} />
+  );
+};
+
+export { Input, Form };
