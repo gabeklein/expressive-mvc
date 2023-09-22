5c1bad34415d20d5dfc59e425ac8c2af24a5515b
Gabe Klein <gabe@termtm.com>
2023-09-22 16:39:03 -0400

    create demo server

---
+++ packages/demo/README.md
@@ -0,0 +1,4 @@
+<h1 align="center">Development server for examples</h1>
+<h3 align="center">Nothing to see here 👀</h3>
+
+This package uses `webpack-dev-server` to provide a config-free server other modules in this repo, namely `examples`, for purposes of embedding and git-based sandbox templates.
---
+++ packages/demo/serve.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+const { resolve } = require("path");
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const {
+  port = 8080,
+  _: [
+    main = "./src/index.tsx"
+  ]
+} = require('simple-argv');
+
+const SERVER_OPTIONS = {
+  port,
+  host: "0.0.0.0",
+  historyApiFallback: true,
+  hot: true
+};
+
+/** @type {Webpack.Configuration} */
+const CONFIG = {
+  mode: "development",
+  entry: resolve(main),
+  stats: "errors-only",
+  output: {
+    path: resolve("./public"),
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".ts",
+      ".jsx",
+      ".tsx"
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: "swc-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(svg|png|jpg|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: `static/[hash:10][ext]`
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: resolve(__dirname, "./index.html")
+    })
+  ]
+}
+
+new WebpackDevServer(SERVER_OPTIONS, Webpack(CONFIG)).start();
