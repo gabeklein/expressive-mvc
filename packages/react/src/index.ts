873fd7ce404a85befd9303abdc6ca9ae946947b1
Gabe Klein <gabe@termtm.com>
2023-10-17 18:01:31 -0400

    namespace improvements in /react

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -9,31 +9,37 @@ type NoVoid<T> = T extends undefined | void ? null : T;
 
 declare module '@expressive/mvc' {
   namespace Model {
-    namespace GetFactory {
-      type Refresh = {
+    namespace get {
+      type Factory<T extends Model, R> = (this: T, current: T, refresh: ForceRefresh) => R;
+
+      type ForceRefresh = {
         /** Request a refresh for current component. */
         (): void;
         
         /**
          * Request a refresh and again after promise either resolves or rejects.
          * 
-         * @returns Promise which resolves, after refresh, to same value as input.
+         * @param waitFor Promise to wait for.
+         * @returns Promise which resolves, after refresh, to same value as `waitFor`.
          */
-        <T = void>(passthru: Promise<T>): Promise<T>;
+        <T = void>(waitFor: Promise<T>): Promise<T>;
     
         /**
          * Request refresh before and after async function.
          * A refresh will occur both before and after the given function.
          * 
-         * Any actions performed before first `await` will occur prior to refresh.
+         * **Note:** Any actions performed before first `await` will occur prior to refresh.
          * 
-         * @returns Promise which resolves, after refresh, return value.
+         * @param invoke Async function to invoke.
+         * @returns Promise which resolves returned value after refresh.
          */
         <T = void>(invoke: () => Promise<T>): Promise<T>;
       };
     }
 
-    type GetFactory<T extends Model, R> = (this: T, current: T, refresh: GetFactory.Refresh) => R;
+    namespace use {
+      type Callback<T extends Model> = (instance: T) => void;
+    }
 
     /** Fetch instance of this class from context. */
     function get <T extends Model> (this: Model.Type<T>, ignore?: true): T;
@@ -41,15 +47,13 @@ declare module '@expressive/mvc' {
     /** Fetch instance of this class optionally. May be undefined, but will never subscribe. */
     function get <T extends Model> (this: Model.Type<T>, required: boolean): T | undefined;
   
-    function get <T extends Model, R> (this: Model.Type<T>, factory: GetFactory<T, Promise<R> | R>): NoVoid<R>;
+    function get <T extends Model, R> (this: Model.Type<T>, factory: get.Factory<T, Promise<R> | R>): NoVoid<R>;
   
-    function get <T extends Model, R> (this: Model.Type<T>, factory: GetFactory<T, null>): NoVoid<R> | null;
-
-    type UseCallback<T extends Model> = (instance: T) => void;
+    function get <T extends Model, R> (this: Model.Type<T>, factory: get.Factory<T, null>): NoVoid<R> | null;
 
     function use <T extends Model> (this: Model.New<T>, apply?: Model.Values<T>, repeat?: boolean): T;
 
-    function use <T extends Model> (this: Model.New<T>, callback?: UseCallback<T>, repeat?: boolean): T;
+    function use <T extends Model> (this: Model.New<T>, callback?: use.Callback<T>, repeat?: boolean): T;
   }
 }
 
