fbeff8367053d99f8e84451b38ffd9464fab9e42
Gabe Klein <gabe@termtm.com>
2023-12-05 09:37:03 -0500

    create tests for Model.as component

---
+++ packages/react/src/component.test.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { act, create } from 'react-test-renderer';
+
+import Model from '.';
+
+it("will update component as values change", async () => {
+  class Test extends Model {
+    foo = "bar";
+
+    constructor(){
+      super();
+      test = this;
+    }
+  }
+
+  const Component = Test.as(props => (
+    <span>{props.foo}</span>
+  ))
+
+  let test: Test;
+  const rendered = create(<Component />);
+
+  expect(rendered.toJSON()).toEqual({
+    type: "span",
+    props: {},
+    children: ["bar"]
+  });
+
+  await act(() => {
+    test!.foo = "baz";
+  })
+
+  rendered.update(<Component />);
+
+  expect(rendered.toJSON()).toEqual({
+    type: "span",
+    props: {},
+    children: ["baz"]
+  });
+});
+
+it("will pass props to model", () => {
+  const didUpdateFoo = jest.fn();
+  
+  class Test extends Model {
+    foo = "foo";
+
+    constructor(){
+      super();
+      this.set(didUpdateFoo);
+    }
+  }
+
+  const Component = Test.as(props => (
+    <span>{props.foo}</span>
+  ))
+
+  const rendered = create(<Component foo="bar" />);
+
+  expect(rendered.toJSON()).toEqual({
+    type: "span",
+    props: {},
+    children: ["bar"]
+  });
+
+  rendered.update(<Component foo="baz" />);
+
+  expect(rendered.toJSON()).toEqual({
+    type: "span",
+    props: {},
+    children: ["baz"]
+  });
+
+  expect(didUpdateFoo).toHaveBeenCalledTimes(1);
+  expect(didUpdateFoo).toHaveBeenCalledWith("foo", { foo: "baz" });
+});
+
+it("will pass untracked props to render", () => {
+  class Test extends Model {
+    foo = "foo";
+  }
+
+  const Component = Test.as<Test, { value: string }>((props) => (
+    <span>{props.value}</span>
+  ))
+
+  const rendered = create(<Component value="foobar" />);
+
+  expect(rendered.toJSON()).toEqual({
+    type: "span",
+    props: {},
+    children: ["foobar"]
+  });
+});
