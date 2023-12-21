bcaa962499c10ff0f91f9bf7fcce6b2729de3649
Gabe Klein <gabe@termtm.com>
2023-12-21 18:20:25 -0500

    optimize useLocal

--- packages/react/src/useLocal.ts
+++ packages/react/src/useLocal.ts
@@ -4,24 +4,14 @@ import { setContext, useContext, useEffect, useState } from './useContext';
 
 export function useLocal <T extends Model> (
   this: Model.New<T>,
-  argument?: Model.Values<T> | ((instance: T) => void),
+  argument?: Model.Argument<T>,
   repeat?: boolean){
 
   const state = useState(() => {
     let enabled: boolean | undefined;
     let local: T;
 
-    function apply(this: T){
-      if(typeof argument == "function")
-        argument(this);
-
-      else if(argument)
-        for(const key in this)
-          if(argument.hasOwnProperty(key))
-            this[key] = (argument as any)[key];
-    }
-
-    const instance = this.new(apply);
+    const instance = this.new(argument);
     const release = instance.get(current => {
       local = current;
 
@@ -29,12 +19,14 @@ export function useLocal <T extends Model> (
         state[1]((x: Function) => x.bind(null));
     });
 
-    return (props?: Model.Values<T> | ((instance: T) => void)) => {
-      argument = props;
-
-      if(repeat && enabled){
+    return (props?: Model.Argument<T>) => {
+      if(repeat && enabled) {
         enabled = false;
-        apply.call(instance);
+
+        if(typeof props == "function")
+          props.call(instance, instance);
+        else if(typeof props == "object")
+          instance.set(props);
 
         const update = instance.set();
 
