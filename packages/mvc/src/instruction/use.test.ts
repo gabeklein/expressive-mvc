d798333206f452615d4be1055dcda9c6bc0637ef
Gabe Klein <gabe@termtm.com>
2023-12-29 08:23:44 -0500

    constrain use instruction

--- packages/mvc/src/instruction/use.test.ts
+++ packages/mvc/src/instruction/use.test.ts
@@ -1,234 +1,23 @@
-import { mockPromise } from '../mocks';
 import { Model } from '../model';
 import { use } from './use';
 
 describe("model", () => {
-  class Child extends Model {
-    value = "foo"
-  }
-  
-  class Parent extends Model {
-    value = "foo";
-    child = use(Child);
-  }
-  
-  it('will subscribe recursively', async () => {
-    const parent = Parent.new();
-    const mock = jest.fn((it: Parent) => {
-      void it.value;
-      void it.child.value;
-    })
-  
-    expect(parent.value).toBe("foo");
-    expect(parent.child.value).toBe("foo");
-  
-    parent.get(mock);
-  
-    parent.value = "bar";
-    await expect(parent).toHaveUpdated();
-    expect(mock).toHaveBeenCalledTimes(2)
-  
-    parent.child.value = "bar";
-    await expect(parent.child).toHaveUpdated();
-    expect(mock).toHaveBeenCalledTimes(3)
-  })
-  
-  it('will subscribe deeply', async () => {
-    class Parent extends Model {
-      value = "foo";
-      empty = undefined;
-      child = use(Child);
-    }
-  
+  it('will run callback on every assign', () => {
+    const callback = jest.fn();
+
     class Child extends Model {
       value = "foo"
-      grandchild = use(GrandChild);
     }
-  
-    class GrandChild extends Model {
-      value = "bar"
-    }
-  
-    const parent = Parent.new();
-    const effect = jest.fn();
-    let promise = mockPromise();
-  
-    parent.get(state => {
-      const { child } = state;
-      const { grandchild } = child;
-  
-      effect(child.value, grandchild.value);
-      promise.resolve();
-    })
-  
-    expect(effect).toBeCalledWith("foo", "bar");
-    effect.mockClear();
-  
-    promise = mockPromise();
-    parent.child.value = "bar";
-    await promise;
-    
-    expect(effect).toBeCalledWith("bar", "bar");
-    effect.mockClear();
-  
-    promise = mockPromise();
-    parent.child = new Child();
-    await promise;
-    
-    expect(effect).toBeCalledWith("foo", "bar");
-    effect.mockClear();
-  
-    promise = mockPromise();
-    parent.child.value = "bar";
-    await promise;
-    
-    expect(effect).toBeCalledWith("bar", "bar");
-    effect.mockClear();
-  
-    promise = mockPromise();
-    parent.child.grandchild.value = "foo";
-    await promise;
-    
-    expect(effect).toBeCalledWith("bar", "foo");
-    effect.mockClear();
-  });
-  
-  it('will accept instance', async () => {
-    class Parent extends Model {
-      child = use(new Child());
-    }
-  
-    const state = Parent.new();
-    const mock = jest.fn((it: Parent) => {
-      void it.child.value;
-    })
-  
-    state.get(mock);
-  
-    expect(state.child.value).toBe("foo");
-  
-    state.child.value = "bar";
-    await expect(state.child).toHaveUpdated();
-  
-    expect(mock).toBeCalledTimes(2);
-    expect(state.child.value).toBe("bar");
-  })
-  
-  it('will run callback', () => {
-    const callback = jest.fn();
   
     class Parent extends Model {
       child = use(Child, callback);
     }
   
-    Parent.new();
-  
+    const parent = Parent.new();
     expect(callback).toBeCalled();
-  })
-  
-  it('will update on new value', async () => {
-    const state = Parent.new();
-    const mock = jest.fn((it: Parent) => {
-      void it.value;
-      void it.child.value;
-    })
-  
-    state.get(mock);
-  
-    expect(state.child.value).toBe("foo");
-  
-    // Will refresh on sub-value change.
-    state.child.value = "bar";
-    await expect(state.child).toHaveUpdated();
-    expect(state.child.value).toBe("bar");
-    expect(mock).toBeCalledTimes(2);
-  
-    // Will refresh on repalcement.
-    state.child = new Child();
-    await expect(state).toHaveUpdated();
-    expect(state.child.value).toBe("foo");
-    expect(mock).toBeCalledTimes(3);
-  
-    // New subscription still works.
-    state.child.value = "bar";
-    await expect(state.child).toHaveUpdated();
-    expect(state.child.value).toBe("bar");
-    expect(mock).toBeCalledTimes(4);
-  })
-  
-  it('will reset if value is undefined', async () => {
-    class Parent extends Model {
-      value = "foo";
-      child = use<Child>();
-    }
-  
-    const state = Parent.new();
-    const mock = jest.fn((it: Parent) => {
-      void it.value;
-  
-      if(it.child)
-        void it.child.value;
-    })
-  
-    state.get(mock);
-  
-    state.child = new Child();
-    await expect(state).toHaveUpdated();
-    expect(mock).toBeCalledTimes(2)
-  
-    // Will refresh on sub-value change.
-    state.child.value = "bar";
-    await expect(state.child).toHaveUpdated();
-    expect(mock).toBeCalledTimes(3);
-  
-    // Will refresh on undefined.
-    state.child = undefined;
-    await expect(state).toHaveUpdated();
-    expect(state.child).toBeUndefined();
-    expect(mock).toBeCalledTimes(4);
-  
-    // Will refresh on repalcement.
-    state.child = new Child();
-    await expect(state).toHaveUpdated();
-    expect(mock).toBeCalledTimes(5);
-  
-    // New subscription still works.
-    state.child.value = "bar";
-    await expect(state.child).toHaveUpdated();
-    expect(mock).toBeCalledTimes(6);
-  })
-  
-  it('will still subscribe if initially undefined', async () => {
-    class Parent extends Model {
-      value = "foo";
-      child = use<Child>();
-    }
-  
-    const state = Parent.new();
-    const mock = jest.fn((it: Parent) => {
-      void it.value;
-  
-      if(it.child)
-        void it.child.value;
-    })
-  
-    state.get(mock);
-    expect(state.child).toBeUndefined();
-  
-    // Will refresh on repalcement.
-    state.child = new Child();
-    await expect(state).toHaveUpdated();
-    expect(mock).toBeCalledTimes(2)
-  
-    // New subscription does work.
-    state.child.value = "bar";
-    await expect(state.child).toHaveUpdated();
-    expect(mock).toBeCalledTimes(3)
-  
-    // Will refresh on deletion.
-    state.child = undefined;
-    await expect(state).toHaveUpdated();
-    expect(mock).toBeCalledTimes(4)
+
+    parent.child = new Child();
+    expect(callback).toBeCalledTimes(2);
   })
   
   it('will only reassign a matching model', () => {
@@ -242,11 +31,26 @@ describe("model", () => {
   
     expect(() => {
       parent.child = Unrelated.new("ID");
-    }).toThrowError(`ID.child expected Model of type Child but got ID.`)
+    }).toThrowError(`ID.child expected Model of type Child but got Unrelated.`)
   
     expect(() => {
       // @ts-expect-error
       parent.child = undefined;
     }).toThrowError(`ID.child expected Model of type Child but got undefined.`)
+  })
+  
+  it('will allow undefined', () => {
+    class Child extends Model {}
+    class Parent extends Model {
+      child = use(Child, false);
+    }
+  
+    const parent = Parent.new("ID");
+
+    expect(parent.child).toBeInstanceOf(Child);
+
+    parent.child = undefined;
+
+    expect(parent.child).toBeUndefined();
   })
 })
--- packages/mvc/src/instruction/use.ts
+++ packages/mvc/src/instruction/use.ts
@@ -1,35 +1,23 @@
 import { Model, update } from '../model';
 import { add } from './add';
 
-/** Create a placeholder for specified Model type. */
-function use <T extends Model> (): T | undefined;
+function use <T extends Model> (Type: Model.Type<T>, required: false): T | undefined;
 
 /** Create a new child instance of model. */
 function use <T extends Model> (Type: Model.Type<T>, ready?: (i: T) => void): T;
 
-/**
- * Use existing model as a child of model assigned to.
- * 
- * Note: If `peer` is not already initialized before parent is
- * (created with `new` as opposed to create method), that model will
- * attach this via `parent()` instruction. It will not, however, if
- * already active.
- **/
-function use <T extends Model> (model: T, ready?: (i: T) => void): T;
-
 function use <T extends Model> (
-  value?: T | Model.Type<T>,
-  argument?: (i: T | undefined) => void){
+  model: Model.Type<T>,
+  argument?: ((i: T | undefined) => void) | boolean){
 
-  return add((property, subject) => {
-    if(typeof value === "function")
-      value = new value();
+  return add((key, subject) => {
+    const value = new model();
 
     function set(next: Model | undefined){
-      if(value instanceof Model && !(next instanceof value.constructor))
-        throw new Error(`${subject}.${property} expected Model of type ${value.constructor} but got ${next}.`)
+      if(next ? !(next instanceof model) : argument !== false)
+        throw new Error(`${subject}.${key} expected Model of type ${model} but got ${next && next.constructor}.`)
 
-      update(subject, property, next);
+      update(subject, key, next);
 
       if(typeof argument == "function")
         argument(next as T);
