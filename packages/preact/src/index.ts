7260723eee86f8ec908987285376b8f43755243f
Gabe Klein <gabe@termtm.com>
2024-05-13 01:24:54 -0400

    add missing Pragma to preact

--- packages/preact/src/index.ts
+++ packages/preact/src/index.ts
@@ -1,6 +1,6 @@
 import { Context } from '@expressive/mvc';
 import { Pragma, Consumer, Provider } from "@expressive/react/adapter";
-import { createContext } from 'preact';
+import { createContext, createElement } from 'preact';
 import { useContext, useEffect, useState } from "preact/compat";
 
 const Lookup = createContext(new Context());
@@ -20,6 +20,10 @@ Pragma.useFactory = (factory) => {
   }));
 
   return state[0];
+}
+
+Pragma.useProvider = (value, children) => {
+  return createElement(Lookup.Provider, { key: value.id, value, children });
 }
 
 export { Lookup, Consumer, Provider };
