6983abff04f7b98fb174a05130736b2e63e34930
Gabe Klein <gabe@termtm.com>
2025-02-25 23:02:29 -0500

    improved build

---
+++ packages/mvc/tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    clean: true,
+    dts: true,
+    entry: ["src/index.ts"],
+    format: ['cjs'],
+    outDir: "dist",
+    sourcemap: true,
+  },
+  {
+    bundle: false,
+    format: ['esm'],
+    outDir: "dist/esm",
+    sourcemap: "inline",
+    entry: [
+      "src/**/*.ts",
+      "!src/**/*.test.ts",
+      "!src/**/mocks.ts"
+    ]
+  }
+]);
