beac2d6b93da26c9837022331c4d90c225068e45
Gabe Klein <gabe@termtm.com>
2025-02-25 23:41:38 -0500

    localize jest setup in react package

---
+++ packages/react/jest.js
@@ -0,0 +1,80 @@
+import { Model } from "@expressive/mvc";
+
+expect.extend({
+  toUpdate,
+  toHaveUpdated
+});
+
+/**
+ * @param {Model} received
+ * @param {number} timeout
+ */
+async function toUpdate(received, timeout = 0){
+  if(!(received instanceof Model))
+    return {
+      pass: false,
+      message: () => `Expected Model but got ${received}.`
+    }
+
+  return new Promise((resolve) => {
+    const remove = received.set(() => {
+      clearTimeout(timer);
+      return () => {
+        remove();
+        resolve({
+          pass: true,
+          message: () => `Expected ${received} not to update.`
+        })
+      };
+    });
+
+    const timer = setTimeout(() => {
+      remove();
+      resolve({
+        pass: false,
+        message: () => `Expected ${received} to update within ${timeout}ms.`
+      });
+    }, timeout);
+  })
+}
+
+/**
+ * @param {Model} received
+ * @param {string[]} keys
+ */
+async function toHaveUpdated(received, ...keys){
+  if(!(received instanceof Model))
+    return {
+      pass: false,
+      message: () => `Expected Model but got ${received}.`
+    }
+
+  const didUpdate = await received.set();
+
+  if(!didUpdate)
+    return {
+      pass: false,
+      message: () => `Expected ${received} to have pending updates.`
+    }
+
+  if(!keys.length)
+    return {
+      pass: true,
+      message: () => `Expected ${received} not to have pending updates.`
+    }
+
+  for(const key of keys)
+    if(!didUpdate.includes(key))
+      return {
+        pass: false,
+        message: () => {
+          return `Expected ${received} to have updated keys [${keys.map(String).join(", ")}] but got [${didUpdate.join(", ")}].`
+        }
+      }
+
+  return {
+    pass: true,
+    message: () =>
+      `Expected ${received} not to have updated keys [${keys.map(String).join(", ")}].`
+  }
+}
