a2c62ffa133dfd4597cc3fa7e20d777c3070c72d
Gabe Klein <gabe@termtm.com>
2025-05-03 22:02:25 -0400

    test: add test for dev-runtime for coverage

--- packages/react/src/jsx-dev-runtime.test.tsx
+++ packages/react/src/jsx-dev-runtime.test.tsx
@@ -1,8 +1,20 @@
-import { jsxDEV, Fragment } from './jsx-dev-runtime';
+import { Model } from '.';
+import { Fragment, jsxDEV } from './jsx-dev-runtime';
 
 it("exports jsxDEV", () => {
   expect(jsxDEV).toBeDefined();
 });
+
+it("will convert Model to element", () => {
+  const element = jsxDEV(Model, { children: "Hello" }, "key", false);
+
+  expect(element).toMatchObject({
+    type: expect.any(Function),
+    props: {
+      children: "Hello"
+    }
+  });
+})
 
 it("exports Fragment", () => {
   expect(Fragment).toBeDefined();
