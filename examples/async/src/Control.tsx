6bd611e9c7addfc73247bbdb2d1b441a77d001e9
Gabe Klein <gabe@termtm.com>
2026-01-20 13:40:03 -0500

    refactor: move timer logic from constructor to new() method for clarity

--- examples/async/src/Control.tsx
+++ examples/async/src/Control.tsx
@@ -5,31 +5,28 @@ export default class Control extends State {
   remaining = 30;
   dead?: boolean = undefined;
 
-  constructor(...args: State.Args) {
-    // Functions passed to constructor are called
-    // when a model becomes ready to be observed.
-    // The function will only be called once and return function
-    // will be called on `null` event, when model is destroyed.
-    super(...args, () => {
-      const done = () => clearInterval(timer);
-      const timer = setInterval(() => {
-        const remains = this.remaining--;
+  // `this.new` is called hen a model becomes ready to be observed.
+  // The method will only be called once and return function
+  // will be called on `null` event, when model is destroyed.
+  new() {
+    const done = () => clearInterval(timer);
+    const timer = setInterval(() => {
+      const remains = this.remaining--;
 
-        if (remains === 0) {
-          this.dead = Math.random() > 0.5;
-          done();
-        }
-      }, 1000);
+      if (remains === 0) {
+        this.dead = Math.random() > 0.5;
+        done();
+      }
+    }, 1000);
 
-      return done;
-    });
+    return done;
   }
 
-  getNewAgent = async () => {
+  async getNewAgent() {
     const res = await fetch('https://randomuser.me/api?nat=gb&results=1');
     const data = await res.json();
     const recruit = data.results[0];
 
     this.agent = recruit.name.last;
-  };
+  }
 }
