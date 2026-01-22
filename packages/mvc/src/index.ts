8196c1c110c9c3fa5dddab93e412338cb7a60ac1
Gabe Klein <gabe@termtm.com>
2026-01-22 15:59:14 -0500

    fix: remove has export from index.ts

--- packages/mvc/src/index.ts
+++ packages/mvc/src/index.ts
@@ -1,8 +1,7 @@
 export { use, Instruction } from './instruction/use';
 export { get } from './instruction/get';
-export { ref } from './instruction/ref';
 export { set } from './instruction/set';
-export { has } from './instruction/has';
+export { ref } from './instruction/ref';
 
 export { State, State as default, METHOD } from './state';
 export { watch, addListener, event, Observable } from './control';
