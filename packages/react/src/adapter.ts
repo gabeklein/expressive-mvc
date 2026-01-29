04beb4bd68ace789f502cecc6949975712867f29
Gabe Klein <gabe@termtm.com>
2026-01-29 08:49:11 -0500

    refactor: export Pragma as a constant for improved accessibility

--- packages/react/src/adapter.ts
+++ packages/react/src/adapter.ts
@@ -1,9 +1,5 @@
-namespace Pragma {}
-
-const Pragma = {} as {
+export const Pragma = {} as {
   useState<S>(initial: () => S): [S, (next: (previous: S) => S) => void];
   useEffect(effect: () => () => void, deps?: any[]): void;
   createElement(type: any, props?: any, ...children: any[]): any;
 };
-
-export { Pragma };
