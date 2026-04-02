a4f3fc328ba31b57d06194bd4e73c295ca856e5f
Gabe Klein <gabe@termtm.com>
2026-04-02 10:10:35 -0400

    refactor(tsdown): remove jsx-runtime entries from configuration

--- packages/react/tsdown.config.ts
+++ packages/react/tsdown.config.ts
@@ -6,13 +6,11 @@ export default defineConfig([
     sourcemap: true,
     clean: true,
     outDir: 'dist',
-    external: ['./state', './jsx-runtime'],
+    external: ['./state'],
     outExtensions: () => ({ js: '.js' }),
     entry: {
       index: 'src/index.ts',
       state: 'src/state.ts',
-      'jsx-runtime': 'src/jsx-runtime.ts',
-      'jsx-dev-runtime': 'src/jsx-dev-runtime.ts'
     },
     format: ['cjs'],
     outputOptions: {
@@ -23,15 +21,13 @@ export default defineConfig([
     sourcemap: true,
     unbundle: true,
     outDir: 'dist/esm',
-    external: ['./state', './jsx-runtime'],
+    external: ['./state'],
     dts: false,
     outExtensions: () => ({ js: '.js' }),
     format: ['esm'],
     entry: {
       index: 'src/index.ts',
       state: 'src/state.ts',
-      'jsx-runtime': 'src/jsx-runtime.ts',
-      'jsx-dev-runtime': 'src/jsx-dev-runtime.ts'
     },
     outputOptions: {
       exports: 'named'
