03247f6654f1a447f86c68fdb4afbe073c372a10
Gabe Klein <gabe@termtm.com>
2026-04-15 20:20:51 -0400

    refactor: update context example to use Component instead of State

--- website/examples/context/App.tsx
+++ website/examples/context/App.tsx
@@ -1,34 +1,31 @@
-import State, { Provider } from '@expressive/react';
+import { Component } from '@expressive/react';
 
 /*
-  When using context the first step is to make some model
-  as you normally would. It can contain values which some
-  components may read and others may write. Shared state
-  is de-facto, easy communication between components!
+  When using context the first step is to make some State
+  as you normally would. Just extend Component instead, it
+  is renderable and will provide to children automatically.
+  It can contain the values which function components may
+  read and others may write. Shared state s de-facto,
+  easy communication between components.
 */
-class FooBar extends State {
+class FooBar extends Component {
   foo = 0;
   bar = 0;
 }
 
-const Demo = () => {
-  /*
-    Expressive Provider is universal; it accepts a `for` prop,
-    which can be a State class, instance, or a collection of either.
-  */
+export default function Demo() {
   return (
-    <Provider for={FooBar}>
+    <Component>
       <Foo />
       <Bar />
-    </Provider>
+    </Component>
   );
-};
+}
 
-const Foo = () => {
-  /*
-    Any model passed to `for` will be available to all children of the
-    provider via the `get` method of that class.
-  */
+function Foo() {
+  // Built-in get() static method is a hook, it returns nearest of
+  // same type and syncs component to any values you access.
+  // `is` property loops back to instance so you can update values.
   const { is: foobar, bar } = FooBar.get();
 
   return (
@@ -36,19 +33,17 @@ const Foo = () => {
       <button
         onClick={() => {
           foobar.foo++;
-        }}>
+        } }>
         Foo
       </button>
       <small>(Bar was clicked {bar} times)</small>
     </div>
   );
-};
+}
 
-const Bar = () => {
-  /*
-    Components which access particlar values will only refresh when those values change.
-    Where `bar` may update many times, this will only update when `foo` changes.
-  */
+function Bar() {
+  // Dependancy tracking is automatic, so this only refreshes
+  // when `foo` changes, but not when `bar` does.
   const { is: foobar, foo } = FooBar.get();
 
   return (
@@ -56,12 +51,10 @@ const Bar = () => {
       <button
         onClick={() => {
           foobar.bar++;
-        }}>
+        } }>
         Bar
       </button>
       <small>(Foo was clicked {foo} times)</small>
     </div>
   );
-};
-
-export default Demo;
+}
