ecd69d8ae588fc7368a68e08f1ebf966a417456f
Gabe Klein <gabe@termtm.com>
2026-04-19 21:53:03 -0400

    feat(website): add App.css for styling and improve comments in counter example

--- website/examples/counter/App.tsx
+++ website/examples/counter/App.tsx
@@ -1,25 +1,23 @@
 import State from '@expressive/react';
 
 // When we create a State, we're defining a set of values to be tracked.
-// It is recommended any State specific to a component is called 'Control'.
+// Recommend any State specific to one component is called 'Control'.
 
 class Control extends State {
-  // Here we only need one property, `current` whose initial value will be 1.
+  // Here only one property needed, `current` whose initial value will be 1.
   // All properties are tracked by a controller, so any changes to a value
   // used by a component, will intelligently re-render that component.
   current = 1;
 
   // For convenience, we define two methods to increment and decrement the
-  // value of `current`. These methods will be passed to the component below.
-  // Assignments to `this` update state so the component will refresh!
+  // value of `current`. Methods can be used by component to decouple logic.
   decrement = () => this.current--;
   increment = () => this.current++;
 }
 
 const App = () => {
-  // Here we create an instance of the model with the `use` method.
-  // It's a hook to create, memoize and observe new instance of Control.
-  // Destructured values tell the component to refresh when changed.
+  // Control.use() is a hook to create, memoize and observe Control.
+  // Destructured values tell the component what to refresh for.
   const { is, current, increment, decrement } = Control.use();
 
   return (
@@ -27,6 +25,7 @@ const App = () => {
       <h1>Counter Example</h1>
       <div className="counter">
         <button onClick={decrement}>{'-'}</button>
+        {/* `is` points to the instance we can assign to directly. */}
         <pre onClick={() => (is.current = 1)}>{current}</pre>
         <button onClick={increment}>{'+'}</button>
       </div>
