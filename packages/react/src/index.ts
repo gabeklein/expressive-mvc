c81829cb307a2f9f45922b1754729aed520bda02
Gabe Klein <gabe@termtm.com>
2026-05-07 08:35:07 -0400

    fix: add 'hot' export to state module

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -10,6 +10,6 @@ Runtime.useState = useState;
 Runtime.useRef = useRef;
 
 export { State, State as default };
-export { Context, Observable, def, get, ref, set } from '@expressive/state';
+export { Context, Observable, def, get, ref, set, hot } from '@expressive/state';
 export { Component } from './component';
 export { Consumer, Provider };
