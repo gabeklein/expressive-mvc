7d968bfec91bfd2e5493046b2d03605ddec196d4
Gabe Klein <gabe@expressive.dev>
2026-06-11 18:15:53 -0400

    feat(router): NavLink active styling

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -2,6 +2,6 @@ export { matchPattern, fullPattern, patternSegment } from './url';
 export type { Match } from './url';
 export { Router, BrowserRouter } from './router';
 export { Route } from './route';
-export { Link } from './link';
+export { Link, NavLink } from './link';
 export { Redirect } from './redirect';
 export { NavLinks } from './nav';
