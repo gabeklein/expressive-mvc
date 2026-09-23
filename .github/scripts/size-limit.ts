a7f16b3aa3f9d85f9e1888fdb00a1f3bd8bd47df
Gabe Klein <gabe@expressive.dev>
2026-09-23 15:41:31 -0400

    chore(jsx): refresh appearance size budgets

--- .github/scripts/size-limit.ts
+++ .github/scripts/size-limit.ts
@@ -45,13 +45,13 @@ const CASES = [
   },
   {
     name: 'jsx: renderer',
-    limit: 15400,
+    limit: 15700,
     code: `import State, { Component, Context, Consumer, Provider, createPortal, def, get, has, lazy, map, pending, ref, render, set } from '@expressive/jsx';
            console.log(State, Component, Context, Consumer, Provider, createPortal, def, get, has, lazy, map, pending, ref, render, set);`
   },
   {
     name: 'jsx: styling',
-    limit: 17400,
+    limit: 17700,
     code: `import * as all from '@expressive/jsx'; console.log(all);`
   },
   {
